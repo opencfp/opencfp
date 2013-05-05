33424a94a9ccab41a2f5c1a173e2a68dbb7b0099
Vic Metcalfe <vic@zymsys.com>
2013-05-05 11:51:16 -0400

    Yeah, that Database constructor was supposed to be a test fixture.

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -4,6 +4,12 @@ namespace TrueNorth\OpenCFP;
 
 
 class DatabaseTest extends \PHPUnit_Framework_TestCase {
+    protected function setUp()
+    {
+        putenv(Configuration::OPENCFP_PDO_DSN);
+        putenv(Configuration::OPENCFP_PDO_USER);
+        putenv(Configuration::OPENCFP_PDO_PASSWORD);
+    }
 
     public function testGetPDO()
     {
--- web/classes/TrueNorth/opencfp/Database.php
+++ web/classes/TrueNorth/opencfp/Database.php
@@ -3,13 +3,6 @@ namespace TrueNorth\OpenCFP;
 
 class Database
 {
-    function __construct()
-    {
-        putenv(Configuration::OPENCFP_PDO_DSN);
-        putenv(Configuration::OPENCFP_PDO_USER);
-        putenv(Configuration::OPENCFP_PDO_PASSWORD);
-    }
-
     public function getPDO()
     {
         $configuration = new Configuration();
