53f1eab1a311517668eb0c2c6e5353fd468bb727
Chris Hartjes <chartjes@littlehart.net>
2013-05-16 16:45:24 -0500

    Added in comment so people remember to change their default user names and passwords

--- web/bootstrap.php
+++ web/bootstrap.php
@@ -7,12 +7,17 @@ define('APP_DIR', __DIR__);
 // start our session
 session_start();
 
-// Database config stuff loaded here
+/**
+ * Database and Sentty configuration done here
+ * CHANGE THIS TO MATCH YOUR PREFERRED USERNAME AND PASSWORD 
+ */
 class_alias('Cartalyst\Sentry\Facades\Native\Sentry', 'Sentry');
 $dsn = "mysql:dbname=cfp;host=localhost";
 $user = "root";
-Sentry::setupDatabaseResolver(new PDO($dsn, $user));
+$password = "";
+$db = new \PDO($dsn, $user, $password);
 
+Sentry::setupDatabaseResolver($db);
 
 // Initialize Twig
 $loader = new Twig_Loader_Filesystem('./templates');
