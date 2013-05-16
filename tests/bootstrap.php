39a36d602adf197768917024db424f5c6fc51a04
Chris Hartjes <chartjes@littlehart.net>
2013-05-16 13:02:25 -0500

    Also removed some bootstrap magic that we don't need

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,8 @@ if (!defined('APP_DIR')) {
 	define('APP_DIR', __DIR__ . '/../web'); 
 }
 
+// Grab our autoloader
 require APP_DIR . '/vendor/autoload.php';
+
+// Load out mock for tests that use PDO
+require './PDOMock.php';
