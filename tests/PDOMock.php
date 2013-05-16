7ee20610c7ac739495ef18cd9a2904275a16c7e5
Chris Hartjes <chartjes@littlehart.net>
2013-05-16 13:03:17 -0500

    Added in code and tests for a Speaker object

---
+++ tests/PDOMock.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Test mock for doing PDO interactions
+ */
+class PDOMock extends \PDO
+{
+	public function __construct() {}	
+}
---
+++ web/classes/OpenCFP/Speaker.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Object representing Speaker information that is not contained
+ * in the Sentry User object
+ */
+
+namespace OpenCFP;
+
+class Speaker
+{
+	protected $_db;
+
+	/**
+	 * Constructor for object
+	 *
+	 * @param PDO $db
+	 */
+	public function __construct($db)
+	{
+		$this->_db = $db;
+	}
+
+	/**
+	 * Create new Speaker record in the database
+	 *
+	 * @param array $data
+	 * @return boolean
+	 */
+	public function create($data)
+	{
+		if (empty($data['user_id']) || empty($data['info'])) {
+			return false;
+		}
+
+		$sql = "INSERT INTO speaker (user_id, info) VALUES (?, ?)";
+		$stmt = $this->_db->prepare($sql);
+		
+		return $stmt->execute(array(
+			$data['user_id'],
+			$data['info']
+			)
+		);
+	}
+
+	/**
+	 * Find info for a speaker given a known Sentry User if
+	 *
+	 * @param integer $user_id
+	 * @return false|array
+	 */
+	public function findByUserId($user_id)
+	{
+		if ((int)$user_id !== $user_id) {
+			return false;
+		}	
+
+		$sql = "SELECT info FROM speaker WHERE user_id = ?";
+		$stmt = $this->_db->prepare($sql);
+		$query = $stmt->execute($user_id);
+		$row = $stmt->fetch();
+
+		if ($row !== false) {
+			return $row;
+		}
+
+		return false;
+	}
+}
