6403ff74730023fbd1b84e041f6b86256a50fff1
Chris Hartjes <chartjes@littlehart.net>
2013-05-18 12:24:59 -0500

    Added in new Talk object and some associated tests

---
+++ classes/OpenCFP/Talk.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Object that represents a talk that a speaker has submitted
+ */
+
+namespace OpenCFP;
+
+class Talk
+{
+	protected $_db;
+
+	/**
+	 * Constructor for the class
+	 *
+	 * @param PDO $db
+	 */
+	public function __construct($db)
+	{
+		$this->_db = $db;
+	}
+
+	/**
+	 * Create a talk when you pass new data in
+	 * 
+	 * @param array $data
+	 * @return boolean
+	 */
+	public function create($data)
+	{
+		$sql = "
+		INSERT INTO talks 
+		(title, description, type, user_id)
+		VALUES (?, ?, ?, ?)
+		";
+		$stmt = $this->_db->prepare($sql);
+		
+		return $stmt->execute(
+			array(
+				$data['title'],
+				$data['description'],
+				$data['type'],
+				$data['user_id']
+			)
+		);
+	}
+
+	/**
+	 * Return a record that matches an ID passed into it
+	 *
+	 * @param int $id
+	 * @return array|false
+	 */
+	public function findById($id)
+	{
+		$sql = "SELECT * FROM talks WHERE id = ?";
+		$stmt = $this->_db->prepare($sql);
+		$stmt->execute(array($id));
+
+		return $stmt->fetch() ?: false;
+	}
+
+	/**
+	 * Return one or more Talk records that match a user ID passed in
+	 *
+	 * @param integer $userId
+	 * @return false|array
+	 */
+	public function findByUserId($userId)
+	{
+		$sql = "SELECT * FROM talks WHERE user_id = ?";
+		$stmt = $this->_db->prepare($sql);
+		$stmt->execute(array($userId));
+
+		return $stmt->fetchAll();
+
+	}
+}
---
+++ tests/TalkTest.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * Set of tests for our Talk object
+ */
+
+class TalkTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * Test that creates a new talk
+	 *
+	 * @test
+	 */
+	public function properlyCreateANewTalk()
+	{
+		// Mock our database connection
+		$stmt = $this->getMockBuilder('StdClass')
+			->setMethods(array('execute'))
+			->getMock();
+		$stmt->expects($this->once())
+			->method('execute')
+			->will($this->returnValue(true));
+
+		$db = $this->getMockBuilder('PDOMock')
+			->setMethods(array('prepare'))
+			->getMock();
+		$db->expects($this->once())
+			->method('prepare')
+			->with($this->stringContains("INSERT INTO talks"))
+			->will($this->returnValue($stmt));
+
+		$talk = new \OpenCFP\Talk($db);
+		$data = array(
+			'title' => "The Awesome Talk of Awesomeoneess",
+			'description' => "This is where a description of the talk would go, how long should it be?",
+			'type' => 'tutorial',
+			'user_id' => 1
+		);
+		$response = $talk->create($data);
+
+		$this->assertTrue(
+			$response,
+			"Did now properly create a talk"
+		);
+	}
+
+	/**
+	 * Verify that findById() returns a record as expected
+	 *
+	 * @test
+	 */
+	public function titleFieldIsValidatedCorrectly()
+	{
+		$info = array(
+			'id' => 2,
+			'title' => "Best talk ever",
+			'description' => "This is out talk description",
+			'type' => 'session',
+			'user_id' => 4
+		);
+
+		$stmt = $this->getMockBuilder('StdClass')
+			->setMethods(array('execute', 'fetch'))
+			->getMock();
+		$stmt->expects($this->once())
+			->method('execute')
+			->will($this->returnValue(true));
+		$stmt->expects($this->once())
+			->method('fetch')
+			->will($this->returnValue($info));
+
+		$db = $this->getMockBuilder('PDOMock')
+			->setMethods(array('prepare'))
+			->getMock();
+		$db->expects($this->once())
+			->method('prepare')
+			->with($this->stringContains("SELECT * FROM talks"))
+			->will($this->returnValue($stmt));	
+
+		$talk = new \OpenCFP\Talk($db);
+		$record = $talk->findById($info['user_id']);
+
+		$this->assertEquals(
+			$info,
+			$record,
+			"Talk::findById() did not return the expected record"
+		);
+	}
+
+	/**
+	 * Verify that findByUserId finds one or more talks by a user
+	 * 
+	 * @test
+	 * @param integer $data 
+	 * @dataProvider findByUserIdProvider
+	 */
+	public function findByUserIdReturnsCorrectRecords($data)
+	{
+		$stmt = $this->getMockBuilder('StdClass')
+			->setMethods(array('execute', 'fetchAll'))
+			->getMock();
+		$stmt->expects($this->once())
+			->method('execute')
+			->will($this->returnValue(true));
+		$stmt->expects($this->once())
+			->method('fetchAll')
+			->will($this->returnValue($data));
+
+		$db = $this->getMockBuilder('PDOMock')
+			->setMethods(array('prepare'))
+			->getMock();
+		$db->expects($this->once())
+			->method('prepare')
+			->with($this->stringContains("SELECT * FROM talks"))
+			->will($this->returnValue($stmt));			
+
+		$talk = new \OpenCFP\Talk($db);
+		$talks = $talk->findByUserId(1);
+
+		$this->assertEquals(
+			$data,
+			$talks,
+			"Did not get the expected talks"
+		);
+	}
+
+	/**
+	 * Data provider for findByUserIdReturnsCorrectRecords
+	 *
+	 * @return array
+	 */
+	public function findByUserIdProvider()
+	{
+		return array(
+			array(
+				array(
+					'id' => 4,
+					'title' => 'Test talk',
+					'description' => 'Test description',
+					'type' => 'session',
+					'user_id' => 1
+				)
+			),
+			array(
+				array(
+					'id' => 4,
+					'title' => 'Test talk',
+					'description' => 'Test description',
+					'type' => 'session',
+					'user_id' => 1
+				),
+				array(
+					'id' => 5,
+					'title' => 'Test tutorial',
+					'description' => 'This is where the description of this tutorial goes',
+					'type' => 'tutorial',
+					'user_id' => 1
+				)
+			)
+		);
+	}
+}
