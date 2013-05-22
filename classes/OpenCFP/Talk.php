476b8dfba00a556cf50b23049e14bb3652887401
Chris Hartjes <chartjes@littlehart.net>
2013-05-21 20:58:49 -0400

    Added in tests for updating talks

--- classes/OpenCFP/Talk.php
+++ classes/OpenCFP/Talk.php
@@ -8,71 +8,101 @@ namespace OpenCFP;
 
 class Talk
 {
-	protected $_db;
+    protected $_db;
 
-	/**
-	 * Constructor for the class
-	 *
-	 * @param PDO $db
-	 */
-	public function __construct($db)
-	{
-		$this->_db = $db;
-	}
+    /**
+     * Constructor for the class
+     *
+     * @param PDO $db
+     */
+    public function __construct($db)
+    {
+        $this->_db = $db;
+    }
 
-	/**
-	 * Create a talk when you pass new data in
-	 * 
-	 * @param array $data
-	 * @return boolean
-	 */
-	public function create($data)
-	{
-		$sql = "
-		INSERT INTO talks 
-		(title, description, type, user_id)
-		VALUES (?, ?, ?, ?)
-		";
-		$stmt = $this->_db->prepare($sql);
-		
-		return $stmt->execute(
-			array(
-				$data['title'],
-				$data['description'],
-				$data['type'],
-				$data['user_id']
-			)
-		);
-	}
+    /**
+     * Create a talk when you pass new data in
+     * 
+     * @param array $data
+     * @return boolean
+     */
+    public function create($data)
+    {
+        $sql = "
+            INSERT INTO talks 
+            (title, description, type, user_id)
+            VALUES (?, ?, ?, ?)
+            ";
+        $stmt = $this->_db->prepare($sql);
 
-	/**
-	 * Return a record that matches an ID passed into it
-	 *
-	 * @param int $id
-	 * @return array|false
-	 */
-	public function findById($id)
-	{
-		$sql = "SELECT * FROM talks WHERE id = ?";
-		$stmt = $this->_db->prepare($sql);
-		$stmt->execute(array($id));
+        return $stmt->execute(
+            array(
+                $data['title'],
+                $data['description'],
+                $data['type'],
+                $data['user_id']
+            )
+        );
+    }
 
-		return $stmt->fetch() ?: false;
-	}
+    /**
+     * Return a record that matches an ID passed into it
+     *
+     * @param int $id
+     * @return array|false
+     */
+    public function findById($id)
+    {
+        $sql = "SELECT * FROM talks WHERE id = ?";
+        $stmt = $this->_db->prepare($sql);
+        $stmt->execute(array($id));
 
-	/**
-	 * Return one or more Talk records that match a user ID passed in
-	 *
-	 * @param integer $userId
-	 * @return false|array
-	 */
-	public function findByUserId($userId)
-	{
-		$sql = "SELECT * FROM talks WHERE user_id = ?";
-		$stmt = $this->_db->prepare($sql);
-		$stmt->execute(array($userId));
+        return $stmt->fetch() ?: false;
+    }
 
-		return $stmt->fetchAll();
+    /**
+     * Return one or more Talk records that match a user ID passed in
+     *
+     * @param integer $userId
+     * @return false|array
+     */
+    public function findByUserId($userId)
+    {
+        $sql = "SELECT * FROM talks WHERE user_id = ?";
+        $stmt = $this->_db->prepare($sql);
+        $stmt->execute(array($userId));
 
-	}
+        return $stmt->fetchAll();
+
+    }
+
+    /**
+     * Update a record using data passed in to it
+     *
+     * @param array $data
+     * @return boolean
+     */
+    public function update($data)
+    {
+        if (empty($data['id'])) {
+            return false;
+        }
+
+        $sql = "UPDATE talks 
+            SET title = ?
+            description = ?,
+            type = ?,
+            WHERE id = ?
+            AND user_id = ?
+        ";
+        $stmt = $this->_db->prepare($sql);
+        
+        return $stmt->execute(array(
+            $data['title'],
+            $data['description'],
+            $data['type'],
+            $data['user_id'],
+            $data['id']
+        ));   
+    }
 }
