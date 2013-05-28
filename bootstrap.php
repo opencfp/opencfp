c3ee2125c322a485ec2aadab01d755252786eaf3
Peter Meth <pmeth@centah.com>
2013-05-28 14:39:00 -0400

    adding a configuration file to customize setup

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,18 @@
 <?php
 
-// Bootstrap for OpenCFP
-require 'vendor/autoload.php';
 define('APP_DIR', __DIR__);
+if (!file_exists(APP_DIR . '/vendor/autoload.php')) {
+	throw new Exception('Autoload file does not exist.  Did you run composer install?');
+}
+
+if (!file_exists(APP_DIR . '/config/config.ini')) {
+	throw new Exception('Config file does not exist.  Did you copy the config.ini.dist to config.ini?');
+}
+
+// Bootstrap for OpenCFP
+require APP_DIR . '/vendor/autoload.php';
+
+$config = parse_ini_file(APP_DIR . '/config/config.ini', true);
 
 // start our session
 session_start();
@@ -12,14 +22,10 @@ session_start();
  * CHANGE THIS TO MATCH YOUR PREFERRED USERNAME AND PASSWORD
  */
 class_alias('Cartalyst\Sentry\Facades\Native\Sentry', 'Sentry');
-$dsn = "mysql:dbname=cfp;host=localhost";
-$user = "root";
-$password = "";
-$db = new \PDO($dsn, $user, $password);
-unset($dsn, $user, $password);
+$db = new \PDO($config['database']['dsn'], $config['database']['user'], $config['database']['password']);
 Sentry::setupDatabaseResolver($db);
 
 // Initialize Twig
-$loader = new Twig_Loader_Filesystem(APP_DIR . '/templates');
+$loader = new Twig_Loader_Filesystem(APP_DIR . $config['twig']['template_dir']);
 $twig = new Twig_Environment($loader);
 
