cfee33d6dee16bae86e080706a36a251b483f3a7
Vic Metcalfe <vic@zymsys.com>
2013-05-30 17:03:54 -0400

    Detabbed the activation version

--- classes/OpenCFP/Speaker.php
+++ classes/OpenCFP/Speaker.php
@@ -8,61 +8,61 @@ namespace OpenCFP;
 
 class Speaker
 {
-	protected $_db;
+    protected $_db;
 
-	/**
-	 * Constructor for object
-	 *
-	 * @param PDO $db
-	 */
-	public function __construct($db)
-	{
-		$this->_db = $db;
-	}
+    /**
+     * Constructor for object
+     *
+     * @param PDO $db
+     */
+    public function __construct($db)
+    {
+        $this->_db = $db;
+    }
 
-	/**
-	 * Create new Speaker record in the database
-	 *
-	 * @param array $data
-	 * @return boolean
-	 */
-	public function create($data)
-	{
-		if (empty($data['user_id']) || empty($data['info'])) {
-			return false;
-		}
+    /**
+     * Create new Speaker record in the database
+     *
+     * @param array $data
+     * @return boolean
+     */
+    public function create($data)
+    {
+        if (empty($data['user_id']) || empty($data['info'])) {
+            return false;
+        }
 
-		$sql = "INSERT INTO speakers (user_id, info) VALUES (?, ?)";
-		$stmt = $this->_db->prepare($sql);
-		
-		return $stmt->execute(array(
-			$data['user_id'],
-			$data['info']
-			)
-		);
-	}
+        $sql = "INSERT INTO speakers (user_id, info) VALUES (?, ?)";
+        $stmt = $this->_db->prepare($sql);
+        
+        return $stmt->execute(array(
+            $data['user_id'],
+            $data['info']
+            )
+        );
+    }
 
-	/**
-	 * Find info for a speaker given a known Sentry User if
-	 *
-	 * @param integer $user_id
-	 * @return false|array
-	 */
-	public function findByUserId($user_id)
-	{
-		if ((int)$user_id !== $user_id) {
-			return false;
-		}	
+    /**
+     * Find info for a speaker given a known Sentry User if
+     *
+     * @param integer $user_id
+     * @return false|array
+     */
+    public function findByUserId($user_id)
+    {
+        if ((int)$user_id !== $user_id) {
+            return false;
+        }    
 
-		$sql = "SELECT info FROM speakers WHERE user_id = ?";
-		$stmt = $this->_db->prepare($sql);
-		$query = $stmt->execute(array($user_id));
-		$row = $stmt->fetch();
+        $sql = "SELECT info FROM speakers WHERE user_id = ?";
+        $stmt = $this->_db->prepare($sql);
+        $query = $stmt->execute(array($user_id));
+        $row = $stmt->fetch();
 
-		if ($row !== false) {
-			return $row;
-		}
+        if ($row !== false) {
+            return $row;
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
--- web/controllers/process_create_user.php
+++ web/controllers/process_create_user.php
@@ -8,38 +8,38 @@ $data = array();
 $pageTemplate = 'create_user.twig';
 
 if ($form->validateAll()) {
-	$sanitizedData = $form->sanitize();
+    $sanitizedData = $form->sanitize();
 
-	// Create account using Sentry
-	$userData = array(
-		'email' => $sanitizedData['email'],
-		'password' => $sanitizedData['password'],
-	);
+    // Create account using Sentry
+    $userData = array(
+        'email' => $sanitizedData['email'],
+        'password' => $sanitizedData['password'],
+    );
 
-	try {
-		$user = Sentry::getUserProvider()->create($userData);
+    try {
+        $user = Sentry::getUserProvider()->create($userData);
 
-		// Add them to the proper group
-		$adminGroup = Sentry::getGroupProvider()->findByName('Speakers');
-		$user->addGroup($adminGroup);
+        // Add them to the proper group
+        $adminGroup = Sentry::getGroupProvider()->findByName('Speakers');
+        $user->addGroup($adminGroup);
 
-		// Create a Speaker record
-		$speaker = new \OpenCFP\Speaker($db);
-		$speaker->create(array(
-		    $user->getId(),
-		    $sanitizedData['speaker_info'])
-		);
+        // Create a Speaker record
+        $speaker = new \OpenCFP\Speaker($db);
+        $speaker->create(array(
+            $user->getId(),
+            $sanitizedData['speaker_info'])
+        );
 
-		$pageTemplate = "create_user_success.twig";
+        $pageTemplate = "create_user_success.twig";
 
         $form->sendActivationMessage($user);
-	} catch (Cartalyst\Sentry\Users\UserExistsException $e) {
-		$data['error_messages'] = array("A user already exists with that email address");
-	}
+    } catch (Cartalyst\Sentry\Users\UserExistsException $e) {
+        $data['error_messages'] = array("A user already exists with that email address");
+    }
 }
 
 if (!$form->validateAll() && empty($data['error_messages'])) {
-	$data['error_messages'] = $form->errorMessages;
+    $data['error_messages'] = $form->errorMessages;
 }
 
 $template = $twig->loadTemplate($pageTemplate);
