e44835be950580a32d0e86907f28a7510acce4b0
Chris Hartjes <chartjes@littlehart.net>
2013-05-30 23:03:49 -0400

    Added in a check to redirect non-authenticated users to the login page

--- web/dashboard.php
+++ web/dashboard.php
@@ -1,7 +1,16 @@
 <?php
 require '../bootstrap.php';
 
-// Set a few options
+/**
+ * Make sure the user is logged in, kicking them out to the login page
+ * if they are not
+ */
+try {
+    $user = Sentry::getUser();
+} catch (Cartalyst\Sentry\Users\UserNotFoundException $e) {
+    header('Location: login.php');
+    exit;
+}
 
 // Load our template and RENDER
 $template = $twig->loadTemplate('dashboard.twig');
