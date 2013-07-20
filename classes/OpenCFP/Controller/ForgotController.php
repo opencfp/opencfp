d72ecdd7910eabddf698f7090cde26c682e32d92
Chris Hartjes <chartjes@littlehart.net>
2013-07-20 17:14:43 -0400

    Added in support for 'forgot my password' along with the ability to reset your password

---
+++ classes/OpenCFP/Controller/ForgotController.php
@@ -0,0 +1,232 @@
+<?php
+namespace OpenCFP\Controller;
+
+use OpenCFP\Config\ConfigINIFileLoader;
+use OpenCFP\Model\User;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ForgotController
+{
+    public function indexAction(Request $req, Application $app)
+    {
+        $form = $this->getForm($app);
+        $template = $app['twig']->loadTemplate('forgot_index.twig');
+
+        return $template->render(array('form' => $form->createView()));
+    }
+
+    public function sendResetAction(Request $req, Application $app)
+    {
+        $form = $this->getForm($app);
+        $form->bind($req);
+
+        if (!$form->isValid()) {
+            $app['session']->set('flash', array(
+                'type' => 'error',
+                'short' => '',
+                'ext' => "Please enter a properly formatted email address"
+            ));
+            
+            return $app->redirect($app['url'] . '/forgot');
+        } 
+
+        // Check to make sure they actually exist in the system...
+        $data = $form->getData();
+
+        try {
+            $user = $app['sentry']->getUserProvider()->findByLogin($data['email']);
+        } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
+            $app['session']->set('flash', array(
+                'type' => 'error',
+                'short' => '',
+                'ext' => "We couldn't find a user with that email"
+            ));
+            
+            return $app->redirect($app['url'] . '/forgot');
+        }
+
+        // Create a reset code and email the URL to our user
+        $reset_code = $user->getResetPasswordCode();
+        $response = $this->sendResetEmail($app['twig'], $user->getId(), $data['email'], $reset_code);
+
+        if ($response == false) {
+            $app['session']->set('flash', array(
+                'type' => 'error',
+                'short' => '',
+                'ext' => "We were unable to send your password reset request. Please try again"
+            ));
+            
+            return $app->redirect($app['url'] . '/forgot');
+        }
+
+        return $app->redirect($app['url'] . '/forgot_success');
+    }
+
+    public function successAction(Request $req, Application $app)
+    {
+        $template = $app['twig']->loadTemplate('forgot_success.twig');
+        
+        return $template->render(array());
+    }
+
+    public function resetAction(Request $req, Application $app)
+    {
+        try {
+            $user = $app['sentry']->getUserProvider()->findById($req->get('user_id'));
+        } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
+            $template = $app['twig']->loadTemplate('bad_reset_code.twig');
+      
+            return $template->render(array());
+        }
+
+        if (!$user->checkResetPasswordCode($req->get('reset_code'))) {
+            $template = $app['twig']->loadTemplate('bad_reset_code.twig');
+       
+            return $template->render(array());
+        }
+
+        // Build password form and display it to the user
+        $form = $this->getResetForm($app, $req->get('user_id'), $req->get('reset_code'));
+        $template = $app['twig']->loadTemplate('reset_password.twig');
+        
+        return $template->render(array('form' => $form->createView()));
+    }
+
+    public function processResetAction(Request $req, Application $app)
+    {
+        $user_id = $req->get('user_id');
+        $reset_code = $req->get('reset_code');
+
+        $form = $this->getResetForm($app, $user_id, $reset_code);
+        $form->bind($req);
+
+        if (!$form->isValid()) {
+            $template = $app['twig']->loadTemplate('reset_password.twig');
+
+            return $template->render(array('form' => $form->createView()));
+        }
+
+        $data = $form->getData();
+        
+        // Make sure the user exists in the system
+        try {
+            $user = $app['sentry']->getUserProvider()->findById($data['user_id']);
+        } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
+            die('User does not exist');
+            $template = $app['twig']->loadTemplate('bad_reset_code.twig');
+      
+            return $template->render(array());
+        }
+        
+        // Make sure they are using a valid code
+        if (!$user->checkResetPasswordCode($data['reset_code'])) {
+            $template = $app['twig']->loadTemplate('bad_reset_code.twig');
+            return $template->render(array());
+        }
+
+
+        /**
+         * Can't let people replace their passwords with one they have
+         * already
+         */
+        if ($user->checkPassword($data['password']) === true) {
+            die('Trying to use same password');
+            $passwordError = new \Symfony\Component\Form\FormError('text', 'Please select a different password than your current one');
+            $form->addError($passwordError);
+            $template = $app['twig']->loadTemplate('reset_password.twig');
+
+            return $template->render(array('form' => $form->createView()));
+        }
+
+
+        //Everything looks good, let's actually reset their password
+        $template_name = 'reset_success.twig';
+
+        if ($user->attemptResetPassword($data['reset_code'], $data['password']) === false) {
+            $template_name = 'reset_failure.twig';
+        }
+
+        $template = $app['twig']-> loadTemplate($template_name);
+
+        return $template->render(array());
+    }
+
+    protected function getForm($app)
+    {
+        $form = $app['form.factory']->createBuilder('form')
+            ->add('email', 'text', array('constraints' => new Assert\Email()))
+            ->getForm();
+
+        return $form;
+    }
+
+    protected function getResetForm($app, $user_id, $reset_code)
+    {
+        $form = $app['form.factory']->createBuilder('form')
+            ->add('password', 'repeated', array(
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                    new Assert\Length(array('min' => 5))),
+                'type' => 'password',
+                'first_options' => array('label' => 'Password (minimum 5 characters)'),
+                'second_options' => array('label' => 'Password (confirm)'),
+                'first_name' => 'passwd',
+                'second_name' => 'passwd2',
+                'invalid_message' => 'Passwords did not match'))
+            ->add('user_id', 'hidden', array('attr' => array('value' => $user_id)))
+            ->add('reset_code', 'hidden', array('attr' => array('value' => $reset_code)))
+            ->getForm();
+
+        return $form;
+    }
+
+    protected function sendResetEmail($twig, $user_id, $email, $reset_code)
+    {
+        // Create our Mailer object
+        $loader = new ConfigINIFileLoader(APP_DIR . '/config/config.ini');
+        $config_data = $loader->load();
+        $transport = new \Swift_SmtpTransport(
+            $config_data['smtp']['host'],
+            $config_data['smtp']['port']
+        );
+        
+        if (!empty($config_data['smtp']['user'])) {
+            $transport->setUsername($config_data['smtp']['user'])
+                      ->setPassword($config_data['smtp']['password']);
+        }
+
+        if (!empty($config_data['smtp']['encryption'])) {
+            $transport->setEncryption($config_data['smtp']['encryption']);
+        }
+
+        $mailer = new \Swift_Mailer($transport);
+        $message = new \Swift_Message();
+
+        // Build our email that we will send
+        $template = $twig->loadTemplate('reset_password_email.twig');
+        $parameters = array(
+            'reset_code' => $reset_code,
+            'method' => (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off')
+                ? 'https' : 'http',
+            'host' => !empty($_SERVER['HTTP_HOST'])
+            ? $_SERVER['HTTP_HOST'] : 'localhost',
+            'user_id' => $user_id
+        );
+        $message->setTo($email);
+        $message->setFrom(
+            $template->renderBlock('from', $parameters),
+            $template->renderBlock('from_name', $parameters)
+        );
+        $message->setSubject($template->renderBlock('subject', $parameters));
+        $message->setBody($template->renderBlock('body_text', $parameters));
+        $message->addPart(
+            $template->renderBlock('body_html', $parameters),
+            'text/html'
+        );
+
+        return $mailer->send($message);
+    }
+}
+
