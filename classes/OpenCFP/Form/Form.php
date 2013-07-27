68fad80e30e5b5a5565bc8775d46ae24bac3c965
Peter Meth <pmeth@rogers.com>
2013-07-27 17:15:10 -0400

    updating base form class with some helper methods.  bringing us closer to #66.

--- classes/OpenCFP/Form/Form.php
+++ classes/OpenCFP/Form/Form.php
@@ -1,7 +1,9 @@
 <?php
 namespace OpenCFP\Form;
 
-abstract class Form {
+abstract class Form
+{
+    protected $_options;
     protected $_data;
     protected $_purifier;
     protected $_sanitized_data;
@@ -13,11 +15,38 @@ abstract class Form {
      * Class constructor
      *
      * @param $data array of form data
+     * @param \HTMLPurifier $purifier
+     * @param $options
      */
-    public function __construct($data, $purifier)
+    public function __construct($data, \HTMLPurifier $purifier, array $options = array())
+    {
+        $this->_purifier    = $purifier;
+        $this->_options     = $options;
+
+        $this->populate($data);
+    }
+
+    /**
+     * Populates the form with default data, clears previously set sanitized data
+     *
+     * @param array $data
+     */
+    public function populate(array $data)
     {
         $this->_data = $data;
-        $this->_purifier = $purifier;
+        $this->_sanitized_data = null;
+    }
+
+    /**
+     * Updates the form's data.
+     *
+     * @param array $data
+     */
+    public function update(array $data)
+    {
+        // The $_data property might have been already set by
+        // the populate() method.
+        $this->_data = array_merge($this->_data, (array) $data);
     }
 
     /**
@@ -36,27 +65,10 @@ abstract class Form {
         return ($foundFields == $this->_field_list);
     }
 
-    abstract public function validateAll($action = 'create');
-
     /**
-     * Method that sanitizes all data
+     * Returns the clean data.
      *
-     */
-    public function sanitize()
-    {
-        $purifier = $this->_purifier;
-        $this->_sanitized_data = array_map(
-            function ($field) use ($purifier) {
-                return $purifier->purify($field);
-            },
-            $this->_data
-        );
-    }
-
-    /**
-     * Return the array containing sanitized data
-     *
-     * @return array
+     * @return array The cleaned data
      */
     public function getSanitizedData()
     {
@@ -64,8 +76,69 @@ abstract class Form {
     }
 
     /**
+     * Returns the value of a tainted data by its name.
+     *
+     * @param string $name The tainted value name
+     * @param mixed $default The default value to return if not set
+     */
+    public function getTaintedField($name, $default = null)
+    {
+        return isset($this->_data[$name]) ? $this->_data[$name] : $default;
+    }
+
+    /**
+     * Returns the tainted data.
+     *
+     * @return array
+     */
+    public function getTaintedData()
+    {
+        return $this->_data;
+    }
+
+    /**
+     * Returns the value of a form's option if set.
+     *
+     * @param string $name The option name
+     * @param mixed|null $default The default value
+     * @return mixed The option value
+     */
+    public function getOption($name, $default = null)
+    {
+        return isset($this->_options[$name]) ? $this->_options[$name] : $default;
+    }
+
+    /**
+     * Validates the form's submitted data.
+     *
+     */
+    abstract public function validateAll($action = 'create');
+
+    /**
+     * Returns the list of error messages.
+     *
+     * @return array
+     */
+    public function getErrorMessages()
+    {
+        return $this->_error_messages;
+    }
+
+    /**
+     * Returns whether or not the form has error messages.
+     *
+     * @return boolean
+     */
+    public function hasErrors()
+    {
+        return count($this->_error_messages) > 0;
+    }
+
+    /**
      * Method that adds error message to our class attribute, making sure to
      * not add anything that is in there already
+     *
+     * @param string $message The error messages to add to the list
      */
     protected function _addErrorMessage($message)
     {
@@ -74,6 +147,31 @@ abstract class Form {
         }
     }
 
-}
+    /**
+     * Method that sanitizes all data
+     *
+     */
+    public function sanitize()
+    {
+        $this->_sanitized_data = $this->_sanitize($this->_data);
+    }
 
-?>
+    /**
+     * Sanitizes all fields that were submitted.
+     *
+     * @param array $taintedData The tainted data
+     * @return array Sanitized data
+     */
+    protected function _sanitize(array $taintedData)
+    {
+        $purifier  = $this->_purifier;
+        $filtered = array_map(
+            function ($field) use ($purifier) {
+                return $purifier->purify($field);
+            },
+            $taintedData
+        );
+
+        return $filtered;
+    }
+}
