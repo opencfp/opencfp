cdec375e94b4e7719e4caf7587b7ce7cf49dea85
Chris Hartjes <chartjes@littlehart.net>
2013-08-14 16:24:12 -0400

    Hacked on tool to update admin users

--- tools/create_admin_user.php
+++ tools/create_admin_user.php
@@ -1,28 +1,54 @@
 <?php
 
-require __DIR__ . '/../bootstrap.php';
+require '../classes/OpenCFP/Bootstrap.php';
 
-use CHH\Optparse;
+$bootstrap = new \OpenCFP\Bootstrap();
+$app = $bootstrap->getApp();
 
-$parser = new Optparse\Parser();
+$parser = new \CHH\Optparse\Parser();
 $parser->addFlag("help");
+$parser->addFlag("update");
+
 $parser->addArgument("email", array('required' => true));
-$parser->addArgument("password", array('required' => true));
+$parser->addArgument("password", array('required' => false));
 
 try {
     $parser->parse();
-} catch (Optparse\Exception $e) {
+} catch (\CHH\Optparse\Exception $e) {
     echo $parser->usage() . "\n";
     exit(1);
 }
 
-$user = Sentry::getUserProvider()->create(array(
+if ($parser['update']) {
+    $user = $app['sentry']->getUserProvider()->findByLogin($parser['email']);
+
+    if ($user->hasAccess('Admin')) {
+        echo "The user {$parser['email']} already has Admin access\n\n";
+        exit(1);
+    }
+
+    $adminGroup = $app['sentry']->getGroupProvider()->findByName('Admin');
+    $user->addGroup($adminGroup);
+    echo "Added {$parser['email']} to the Admin group\n\n";
+    exit(0);
+}
+
+/**
+ * If we are creating a new account we have to make sure that we have a 
+ * password
+ */
+if ($parser['password'] == '') {
+    echo "When creating new accounts, a password is required";
+    exit(1);
+}
+
+$user = $app['sentry']->getUserProvider()->create(array(
     'email' => $parser['email'],
 		'password' => $parser['password'],
 		'activated' => 1
 ));
 
-$adminGroup = Sentry::getGroupProvider()->findByName('Admin');
+$adminGroup = $app['sentry']->getGroupProvider()->findByName('Admin');
 $user->addGroup($adminGroup);
 
 echo "Done\n";
