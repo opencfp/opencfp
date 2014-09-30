d53587cade26042720184dbf7ed9024c2d958dea
Dustin Wheeler <mdwheele@ncsu.edu>
2014-09-29 18:04:59 -0700

    Updates to build script for Travis.

--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -6,6 +6,8 @@ TRAVIS_DB="cfp_travis"
 # Get a path to the project's root.
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
+# FIXME: Set up cache and upload directory permissions.
+
 # Set up testing database.
 echo "Setting up testing database..."
 mysql -e "DROP DATABASE IF EXISTS $TRAVIS_DB" -uroot
