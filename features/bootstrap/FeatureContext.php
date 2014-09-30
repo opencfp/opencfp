1e3a39fdfcb1b3b82d5af06a1cc51302b7b8af86
Dustin Wheeler <mdwheele@ncsu.edu>
2014-09-29 19:09:57 -0700

    Adds a naive implementation of Behat suite.

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,12 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+class FeatureContext extends MinkContext
+{
+
+}
