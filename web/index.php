573060aaf9880730be30edb5dc4adede107107bd
Dustin Wheeler <mdwheele@ncsu.edu>
2014-11-16 22:20:48 -0500

    Moves namespaces and removes optparse as a dependency to begin work with symfony/console.

--- web/index.php
+++ web/index.php
@@ -1,9 +1,9 @@
 <?php
 
+require_once '../vendor/autoload.php';
+
 use OpenCFP\Application;
 use OpenCFP\Environment;
-
-require_once '../vendor/autoload.php';
 
 $basePath = realpath(dirname(__DIR__));
 $environment = Environment::fromEnvironmentVariable();
