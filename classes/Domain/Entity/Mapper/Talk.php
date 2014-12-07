98658f6474a0d685cbd5b40758f78967a1685df2
Jake Smith <jake.smith92@gmail.com>
2014-12-07 17:13:18 -0600

    Add filtered talk methods (favorited, rated, viewed, etc.)

--- classes/Domain/Entity/Mapper/Talk.php
+++ classes/Domain/Entity/Mapper/Talk.php
@@ -7,17 +7,38 @@ use Spot\Mapper;
 class Talk extends Mapper
 {
     /**
+     * Column Sort By White List
+     * @var array
+     */
+    protected $sort_by_whitelist = [
+        'created_at',
+        'title',
+    ];
+
+    /**
      * Return an array of talks for use by PagerFanta where we set the
      * value in the favorite based on whether or not the current admin
      * user has favourited this talk
      *
-     * @param  integer $admin_user_id
+     * @param integer $admin_user_id
+     * @param string $order_by
+     * @param string $sort Sort Direction
+     * @throws InvalidArgumentException If order by is not in white list
      * @return array
      */
-    public function getAllPagerFormatted($admin_user_id, $sort)
+    public function getAllPagerFormatted($admin_user_id, $options = [])
     {
+        // Merge options with default options
+        $options = $this->getSortOptions(
+            $options,
+            [
+                'order_by' => 'created_at',
+                'sort' => 'DESC',
+            ]
+        );
+
         $talks = $this->all()
-            ->order(['created_at' => 'DESC'])
+            ->order([$options['order_by'] => $options['sort']])
             ->with(['favorites', 'comments']);
         $formatted = array();
 
@@ -53,26 +74,31 @@ class Talk extends Mapper
     /**
      * Return a collection of talks that a majority of the admins have liked
      *
-     * @param  integer $admin_majority
+     * @param integer $admin_user_id
+     * @param array $options Ordery By and Sorting Options
      * @return array
      */
-    public function getAdminFavorites($admin_user_id, $admin_majority)
+    public function getFavoritesByUserId($admin_user_id, $options = [])
     {
-        $talks = $this->all()
-            ->order(['created_at' => 'DESC'])
-            ->with(['favorites']);
+        // Merge options with default options
+        $options = $this->getSortOptions(
+            $options,
+            [
+                'order_by' => 'f.created',
+                'sort' => 'DESC',
+            ]
+        );
 
-        $favorite_talks = [];
-
-        foreach ($talks as $talk) {
-            if ($talk->favorites->count() >= $admin_majority) {
-                $favorite_talks[] = $talk;
-            }
-        }
+        $talks = $this->query(
+            "SELECT t.* FROM talks t "
+            . "LEFT JOIN favorites f ON t.id = f.talk_id "
+            . "WHERE f.admin_user_id = :user_id "
+            . "ORDER BY {$options['order_by']} {$options['sort']}",
+            ['user_id' => $admin_user_id]
+        );
 
         $formatted = [];
-
-        foreach ($favorite_talks as $talk) {
+        foreach ($talks as $talk) {
             $formatted[] = $this->createdFormattedOutput($talk, $admin_user_id);
         }
 
@@ -83,18 +109,209 @@ class Talk extends Mapper
      * Return a collection of top rated talks ordered by rating
      * the talk rating must be above 0 to show on list
      *
-     * @param  integer $admin_user_id
+     * @param integer $admin_user_id
+     * @param array $options Ordery By and Sorting Options
      * @return array
      */
-    public function getTopRatedTalks($admin_user_id)
+    public function getTopRatedByUserId($admin_user_id, $options = [])
     {
+        // Merge options with default options
+        $options = $this->getSortOptions(
+            $options,
+            [
+                'order_by' => 'rating',
+                'sort' => 'DESC',
+            ]
+        );
+
         $talks = $this->query(
             "SELECT t.*, SUM(m.rating) FROM talks t "
             . "LEFT JOIN talk_meta m ON t.id = m.talk_id "
             . "WHERE rating > 0 "
             . "GROUP BY m.`talk_id` "
-            . "ORDER BY rating DESC"
+            . "ORDER BY {$options['order_by']} {$options['sort']}",
+            ['user_id' => $admin_user_id]
         );
+
+        $formatted = [];
+        foreach ($talks as $talk) {
+            $formatted[] = $this->createdFormattedOutput($talk, $admin_user_id);
+        }
+
+        return $formatted;
+    }
+
+    /**
+     * Return a collection of talks not viewed by admin
+     *
+     * @param integer $admin_user_id
+     * @param array $options Ordery By and Sorting Options
+     * @return array
+     */
+    public function getNotViewedByUserId($admin_user_id, $options = [])
+    {
+        // Merge options with default options
+        $options = $this->getSortOptions(
+            $options,
+            [
+                'order_by' => 't.created_at',
+                'sort' => 'DESC',
+            ]
+        );
+
+        $talks = $this->query(
+            "SELECT t.* FROM talks t "
+            . "LEFT JOIN talk_meta m ON t.id = m.talk_id "
+            . "WHERE (m.viewed = 0 AND m.admin_user_id = :user_id) OR m.viewed IS NULL "
+            . "ORDER BY {$options['order_by']} {$options['sort']}",
+            ['user_id' => $admin_user_id]
+        );
+
+        $formatted = [];
+        foreach ($talks as $talk) {
+            $formatted[] = $this->createdFormattedOutput($talk, $admin_user_id);
+        }
+
+        return $formatted;
+    }
+
+    /**
+     * Return a collection of talks viewed by admin
+     *
+     * @param integer $admin_user_id
+     * @param array $options Ordery By and Sorting Options
+     * @return array
+     */
+    public function getViewedByUserId($admin_user_id, $options = [])
+    {
+        // Merge options with default options
+        $options = $this->getSortOptions(
+            $options,
+            [
+                'order_by' => 't.created_at',
+                'sort' => 'DESC',
+            ]
+        );
+
+        $talks = $this->query(
+            "SELECT t.* FROM talks t "
+            . "RIGHT JOIN talk_meta m ON t.id = m.talk_id "
+            . "WHERE m.admin_user_id = :user_id AND m.viewed = 1 "
+            . "ORDER BY {$options['order_by']} {$options['sort']}",
+            ['user_id' => $admin_user_id]
+        );
+
+        $formatted = [];
+        foreach ($talks as $talk) {
+            $formatted[] = $this->createdFormattedOutput($talk, $admin_user_id);
+        }
+
+        return $formatted;
+    }
+
+    /**
+     * Return a collection of talks rated by admin
+     *
+     * @param integer $admin_user_id
+     * @param array $options Ordery By and Sorting Options
+     * @return array
+     */
+    public function getRatedByUserId($admin_user_id, $options = [])
+    {
+        // Merge options with default options
+        $options = $this->getSortOptions(
+            $options,
+            [
+                'order_by' => 'm.created',
+                'sort' => 'DESC',
+            ]
+        );
+
+        $talks = $this->query(
+            "SELECT t.* FROM talks t "
+            . "RIGHT JOIN talk_meta m ON t.id = m.talk_id "
+            . "WHERE m.admin_user_id = :user_id AND (m.rating = 1 OR m.rating = -1) "
+            . "ORDER BY {$options['order_by']} {$options['sort']}",
+            ['user_id' => $admin_user_id]
+        );
+
+        $formatted = [];
+        foreach ($talks as $talk) {
+            $formatted[] = $this->createdFormattedOutput($talk, $admin_user_id);
+        }
+
+        return $formatted;
+    }
+
+    /**
+     * Return a collection of talks not rated by admin
+     *
+     * @param integer $admin_user_id
+     * @param array $options Ordery By and Sorting Options
+     * @return array
+     */
+    public function getNotRatedByUserId($admin_user_id, $options = [])
+    {
+        // Merge options with default options
+        $options = $this->getSortOptions(
+            $options,
+            [
+                'order_by' => 't.created_at',
+                'sort' => 'DESC',
+            ]
+        );
+
+        $talks = $this->query(
+            "SELECT t.* FROM talks t "
+            . "LEFT JOIN talk_meta m ON t.id = m.talk_id "
+            . "WHERE (m.rating = 0 AND m.admin_user_id = :user_id) OR m.rating IS NULL "
+            . "ORDER BY {$options['order_by']} {$options['sort']}",
+            ['user_id' => $admin_user_id]
+        );
+
+        $formatted = [];
+        foreach ($talks as $talk) {
+            $formatted[] = $this->createdFormattedOutput($talk, $admin_user_id);
+        }
+
+        return $formatted;
+    }
+
+    /**
+     * Get a collection of talks filtered by a specific column
+     *
+     * @param string $column Column to filter results with
+     * @param mixed $value Column value
+     * @param integer $admin_user_id
+     * @param array $options Ordery By and Sorting Options
+     * @throws If column is not in the column white list
+     * @return array
+     */
+    public function getTalksFilteredBy($column, $value, $admin_user_id, $options = [])
+    {
+        // Merge options with default options
+        $options = array_merge(
+            $options,
+            [
+                'order_by' => 'created_at',
+                'sort' => 'DESC',
+            ]
+        );
+
+        $column_white_list = [
+            'category',
+            'type',
+            'level',
+        ];
+
+        if (!in_array($column, $column_white_list)) {
+            throw new InvalidArgumentException('Invalid Order By Column ' . $options['order_by']);
+        }
+
+        $talks = $this->all()
+            ->with(['favorites', 'meta'])
+            ->where([$column => $value])
+            ->order([$options['order_by'] => $options['sort']]);
 
         $formatted = [];
         foreach ($talks as $talk) {
@@ -157,5 +374,25 @@ class Talk extends Mapper
         }
 
         return $output;
+    }
+
+    /**
+     * Get sorting options, order_by and sort direction
+     *
+     * @param array $options Sorting Options to Apply
+     * @param array $defaultOptions Default Sorting Options
+     * @return array
+     */
+    protected function getSortOptions(array $options, array $defaultOptions)
+    {
+        if (!in_array($options['order_by'], $this->sort_by_whitelist)) {
+            $options['order_by'] = $defaultOptions['order_by'];
+        }
+
+        if (!in_array($options['sort'], ['ASC', 'DESC'])) {
+            $options['sort'] = $defaultOptions['sort'];
+        }
+
+        return array_merge($defaultOptions, $options);
     }
 }
