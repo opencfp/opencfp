1395674f81ac387fe67570ca72f1fac27dc70f0d
Jake Smith <jake.smith92@gmail.com>
2014-12-07 18:00:02 -0600

    Alter the talk columns to allow NULL values and set specific defaults

---
+++ migrations/20141203111136_alter_talk_column_defaults.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AlterTalkColumnDefaults extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->table('talks')
+            ->changeColumn('desired', 'boolean', ['default' => 0])
+            ->changeColumn('favorite', 'boolean', ['default' => 0])
+            ->changeColumn('sponsor', 'boolean', ['default' => 0])
+            ->changeColumn('selected', 'boolean', ['default' => 0])
+            ->changeColumn('created_at', 'datetime', ['default' => '0000-00-00 00:00:00'])
+            ->changeColumn('updated_at', 'datetime', ['default' => '0000-00-00 00:00:00'])
+            ->save();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->table('talks')
+            ->changeColumn('desired', 'boolean')
+            ->changeColumn('favorite', 'boolean')
+            ->changeColumn('sponsor', 'boolean')
+            ->changeColumn('selected', 'boolean')
+            ->changeColumn('created_at', 'datetime')
+            ->changeColumn('updated_at', 'datetime')
+            ->save();
+    }
+}
