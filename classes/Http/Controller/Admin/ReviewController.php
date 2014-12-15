d4b885132334f6f18c0f794e3fb7b0f46a7af028
Jake Smith <jake.smith92@gmail.com>
2014-12-15 09:24:20 -0600

    Add per page option for talk reviews page

--- classes/Http/Controller/Admin/ReviewController.php
+++ classes/Http/Controller/Admin/ReviewController.php
@@ -20,22 +20,27 @@ class ReviewController extends BaseController
             'order_by' => $req->get('order_by'),
             'sort' => $req->get('sort'),
         ];
+
+        $per_page = (int) $req->get('per_page') ?: 20;
         $talks = $mapper->getTopRatedByUserId($user->id, $options);
 
         // Set up our page stuff
         $adapter = new \Pagerfanta\Adapter\ArrayAdapter($talks);
         $pagerfanta = new \Pagerfanta\Pagerfanta($adapter);
-        $pagerfanta->setMaxPerPage(20);
+        $pagerfanta->setMaxPerPage($per_page);
         $pagerfanta->getNbResults();
 
         if ($req->get('page') !== null) {
             $pagerfanta->setCurrentPage($req->get('page'));
         }
 
+        $queryParams = $req->query->all();
         // Create our default view for the navigation options
-        $routeGenerator = function ($page) {
-            return '/admin/review?page=' . $page;
+        $routeGenerator = function ($page) use ($queryParams) {
+            $queryParams['page'] = $page;
+            return '/admin/talks?' . http_build_query($queryParams);
         };
+
         $view = new TwitterBootstrap3View();
         $pagination = $view->render(
             $pagerfanta,
@@ -47,7 +52,12 @@ class ReviewController extends BaseController
             'pagination' => $pagination,
             'talks' => $pagerfanta,
             'page' => $pagerfanta->getCurrentPage(),
-            'totalRecords' => count($talks)
+            'totalRecords' => count($talks),
+            'per_page' => $per_page,
+            'filter' => $req->get('filter'),
+            'per_page' => $per_page,
+            'sort' => $req->get('sort'),
+            'order_by' => $req->get('order_by'),
         ];
 
         return $this->render('admin/review/index.twig', $template_data);
