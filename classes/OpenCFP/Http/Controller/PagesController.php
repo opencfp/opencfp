d7efbfa756b142a82611adcd624effbe836ae94c
Dustin Wheeler <mdwheele@ncsu.edu>
2015-03-03 19:52:03 -0800

    Adds rudimentary indication that CFP has closed to homepage with submission count.

--- classes/OpenCFP/Http/Controller/PagesController.php
+++ classes/OpenCFP/Http/Controller/PagesController.php
@@ -6,16 +6,24 @@ class PagesController extends BaseController
 {
     public function showHomepage()
     {
-        return $this->render('home.twig');
+        return $this->render('home.twig', $this->getContextWithTalksCount());
     }
 
     public function showSpeakerPackage()
     {
-        return $this->render('package.twig');
+        return $this->render('package.twig', $this->getContextWithTalksCount());
     }
 
     public function showTalkIdeas()
     {
-        return $this->render('ideas.twig');
+        return $this->render('ideas.twig', $this->getContextWithTalksCount());
+    }
+
+    private function getContextWithTalksCount()
+    {
+        $numberOfTalks = $this->app['spot']->mapper('OpenCFP\Domain\Entity\Talk')->all()->count();
+        return [
+            'number_of_talks' => $numberOfTalks
+        ];
     }
 }
