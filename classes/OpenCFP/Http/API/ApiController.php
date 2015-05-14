afe9ac85608d251e2a63e03018f694655ac2c152
Dustin Wheeler <mdwheele@ncsu.edu>
2015-05-14 12:11:31 -0700

    Add docblocks to base controller for API

--- classes/OpenCFP/Http/API/ApiController.php
+++ classes/OpenCFP/Http/API/ApiController.php
@@ -3,29 +3,26 @@
 namespace OpenCFP\Http\API;
 
 use OpenCFP\Application;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\JsonResponse as Response;
 
 class ApiController 
 {
-    /**
-     * @var Application
-     */
-    protected $app;
-
     protected $statusCode = Response::HTTP_OK;
 
-    public function __construct(Application $app)
-    {
-        $this->app = $app;
-    }
-
+    /**
+     * @param $status
+     *
+     * @return $this
+     */
     public function setStatusCode($status)
     {
         $this->statusCode = $status;
         return $this;
     }
 
+    /**
+     * @return int
+     */
     public function getStatusCode()
     {
         return $this->statusCode;
@@ -44,26 +41,51 @@ class ApiController
         return new Response($data, $this->getStatusCode(), $headers);
     }
 
+    /**
+     * @param string $message
+     *
+     * @return mixed
+     */
     public function respondUnauthorized($message = 'Unauthorized')
     {
         return $this->setStatusCode(Response::HTTP_UNAUTHORIZED)->respondWithError($message);
     }
 
+    /**
+     * @param string $message
+     *
+     * @return mixed
+     */
     public function respondForbidden($message = 'Forbidden')
     {
         return $this->setStatusCode(Response::HTTP_FORBIDDEN)->respondWithError($message);
     }
 
+    /**
+     * @param string $message
+     *
+     * @return mixed
+     */
     public function respondNotFound($message = 'Resource not found')
     {
         return $this->setStatusCode(Response::HTTP_NOT_FOUND)->respond($message);
     }
 
+    /**
+     * @param string $message
+     *
+     * @return mixed
+     */
     public function respondInternalError($message = 'Internal server error')
     {
         return $this->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR)->respondWithError($message);
     }
 
+    /**
+     * @param $message
+     *
+     * @return mixed
+     */
     public function respondWithError($message)
     {
         if ($this->statusCode === Response::HTTP_OK) {
@@ -75,16 +97,5 @@ class ApiController
         return $this->respond([
             'message' => $message,
         ]);
-    }
-
-    /**
-     * @param string $route  Route name to redirect to
-     * @param int    $status
-     *
-     * @return RedirectResponse
-     */
-    public function redirectTo($route, $status = 302)
-    {
-        return $this->app->redirect($this->url($route), $status);
     }
 } 
