ee4ea4c84e2f27230ca288013d02ae7fdb04979e
Dustin Wheeler <mdwheele@ncsu.edu>
2015-05-16 20:30:56 -0700

    Add a super insecure identity provider used for the purposes of developing API endpoints

--- classes/OpenCFP/Infrastructure/Auth/UhhhmIdentityProvider.php
+++ classes/OpenCFP/Infrastructure/Auth/UhhhmIdentityProvider.php
@@ -5,9 +5,26 @@ namespace OpenCFP\Infrastructure\Auth;
 use OpenCFP\Domain\Entity\User;
 use OpenCFP\Domain\Services\IdentityProvider;
 use OpenCFP\Domain\Services\NotAuthenticatedException;
+use OpenCFP\Domain\Speaker\SpeakerRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class UhhhmIdentityProvider implements IdentityProvider
 {
+
+    /**
+     * @var SpeakerRepository
+     */
+    private $repository;
+    /**
+     * @var Request
+     */
+    private $request;
+
+    public function __construct(Request $request, SpeakerRepository $repository)
+    {
+        $this->repository = $repository;
+        $this->request = $request;
+    }
 
     /**
      * Retrieves the currently authenticate user's username.
@@ -18,6 +35,6 @@ class UhhhmIdentityProvider implements IdentityProvider
      */
     public function getCurrentUser()
     {
-
+        return $this->repository->findById($this->request->get('auth'));
     }
 }
