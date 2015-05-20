603f0f1a00e51c5fd47b8637d37734f1fddde3a1
Chris Hartjes <chartjes@littlehart.net>
2015-05-20 16:24:34 -0500

    Added test for OpenCFP/Domain/Entity/Talk::getRecent()

--- tests/OpenCFP/Domain/Entity/TalkTest.php
+++ tests/OpenCFP/Domain/Entity/TalkTest.php
@@ -41,4 +41,40 @@ class TalkEntityTest extends \PHPUnit_Framework_TestCase
             'UTF-8 characters were incorrectly encoded'
         );
     }
+
+    /**
+     * @test
+     */
+    public function getRecentFindsMostRecentTalks()
+    {
+        // Create a favorites table, can be empty
+        $favorite_mapper = $this->app['spot']->mapper('OpenCFP\Domain\Entity\Favorite');
+        $favorite_mapper->migrate();
+
+        // Create users entity
+        $user_mapper = $this->app['spot']->mapper('OpenCFP\Domain\Entity\User');
+        $user_mapper->migrate();
+
+        // Create 11 talks
+        $this->bulkCreateTalks(11);
+        $recent_talks = $this->mapper->getRecent(1);
+
+        $this->assertTrue(
+            count($recent_talks) == 10,
+            "Talk::getRecent() did not grab 10 talks out of 11"
+        );
+    }
+
+    private function bulkCreateTalks($numTalks)
+    {
+        for ($x = 1; $x <= $numTalks; $x++) {
+            $title = uniqid();
+            $data = [
+                'title' => $title,
+                'description' => "Description for $title",
+                'user_id' => 1
+            ];
+            $this->mapper->create($data);
+        }
+    }
 }
