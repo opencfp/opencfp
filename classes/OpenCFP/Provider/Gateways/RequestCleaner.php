e1d9070494b7d30da57de7d05fd8c101494139be
Dustin Wheeler <mdwheele@ncsu.edu>
2015-05-22 12:30:58 -0700

    Add invokable RequestCleaner to be used as a before middleware.
    Executes HTMLPurifier on $_GET and $_POST; mutating Symfony Request object.

---
+++ classes/OpenCFP/Provider/Gateways/RequestCleaner.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OpenCFP\Provider\Gateways;
+
+use HTMLPurifier;
+
+class RequestCleaner
+{
+    /**
+     * @var HTMLPurifier
+     */
+    private $purifier;
+
+    public function __construct(HTMLPurifier $purifier)
+    {
+        $this->purifier = $purifier;
+    }
+
+    public function __invoke(Request $request, Application $app){
+        $request->query->replace($this->clean($request->query->all()));
+        $request->request->replace($this->clean($request->request->all()));
+    }
+
+    private function clean(array $data)
+    {
+        $sanitized = [];
+
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                $sanitized[$key] = $this->clean($value);
+            } else {
+                $sanitized[$key] = $this->purifier->purify($value);;
+            }
+        }
+
+        return $sanitized;
+    }
+}
