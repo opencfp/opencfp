9c3d5ea1b6818d953c19f7aada46a22d8ed7bc1f
Andreas Möller <am@localheinz.com>
2015-07-06 22:58:43 -0400

    Fix: Create database in before_script section
    Don't worry about dropping it.

--- tools/travis/reset-database.sh
+++ tools/travis/reset-database.sh
@@ -3,11 +3,6 @@
 # Get a path to the project's root.
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-# Set up testing database.
-echo "Setting up testing database..."
-mysql -e "DROP DATABASE IF EXISTS $TRAVIS_DB" -uroot
-mysql -e "CREATE DATABASE $TRAVIS_DB" -uroot
-
 # Run Phinx migrations.
 echo "Running migrations..."
 $PROJECT_ROOT/vendor/bin/phinx --configuration="$PROJECT_ROOT/phinx.yml" migrate -e testing
