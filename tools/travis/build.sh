2ed8cfe2176611b880156ef51df0107fcb726286
Andreas Möller <am@localheinz.com>
2015-07-06 23:00:52 -0400

    Fix: Run migrations in before_script section
    Don't worry about renaming the migration configuration file.

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -3,6 +3,4 @@
 # Get a path to the project's root.
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-sh $PROJECT_ROOT/tools/travis/reset-database.sh
-
 phpunit --coverage-clover build/logs/clover.xml
--- tools/travis/reset-database.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Get a path to the project's root.
-PROJECT_ROOT=$(git rev-parse --show-toplevel)
-
-# Run Phinx migrations.
-echo "Running migrations..."
-$PROJECT_ROOT/vendor/bin/phinx --configuration="$PROJECT_ROOT/phinx.yml" migrate -e testing
