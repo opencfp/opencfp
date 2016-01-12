74c484c0574c9bb99cf0c238e095b28eef1fbf2e
Michael C <m@michaelcullum.com>
2016-01-12 00:28:58 +0000

    Fix variable name

--- classes/Http/Controller/TalkController.php
+++ classes/Http/Controller/TalkController.php
@@ -21,7 +21,7 @@ class TalkController extends BaseController
      */
     public function isCfpOpen($current_time)
     {
-        if ($currentTime < strtotime($this->app->config('application.enddate') . ' 11:59 PM')) {
+        if ($current_time < strtotime($this->app->config('application.enddate') . ' 11:59 PM')) {
             return true;
         }
 
