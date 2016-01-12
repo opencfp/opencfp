5ffa0264579ba24a7ff121b507dce3cf3a50eacf
Michael C <m@michaelcullum.com>
2016-01-12 00:24:49 +0000

    Fix CFP closing time

--- classes/Http/Controller/TalkController.php
+++ classes/Http/Controller/TalkController.php
@@ -21,7 +21,7 @@ class TalkController extends BaseController
      */
     public function isCfpOpen($current_time)
     {
-        if ($current_time < strtotime($this->app->config('application.enddate'))) {
+        if ($currentTime < strtotime($this->app->config('application.enddate') . ' 11:59 PM')) {
             return true;
         }
 
