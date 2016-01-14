8f001ccbce92a021ee5bda028db261bf47a1e4d6
Dustin Wheeler <mdwheele@ncsu.edu>
2016-01-14 17:27:38 -0500

    Add immutable AirportInfo value object

---
+++ classes/Domain/AirportInfo.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace OpenCFP\Domain;
+
+use Exception;
+
+class AirportInfo
+{
+    /**
+     * @var string IATA Airport Code
+     * @see https://en.wikipedia.org/wiki/International_Air_Transport_Association_airport_code
+     */
+    private $code;
+
+    /**
+     * @var string Name of the airport
+     */
+    private $name;
+
+    /**
+     * @var string Rough string representation of country
+     */
+    private $country;
+
+    private function __construct($code, $name, $country)
+    {
+        if (empty($code) || empty($name) || empty($country)) {
+            throw new Exception('Airport must have code, name and country.');
+        }
+
+        $this->code = $code;
+        $this->name = $name;
+        $this->country = $country;
+    }
+
+    public static function make($code, $name, $country)
+    {
+        return new static($code, $name, $country);
+    }
+
+    public static function fromData(array $data)
+    {
+        return new static(
+            $code = isset($data['code']) ? $data['code'] : null,
+            $name = isset($data['name']) ? $data['name'] : null,
+            $country = isset($data['country']) ? $data['country'] : null
+        );
+    }
+
+    public function __set($name, $value)
+    {
+        throw new Exception('AirportInfo is immutable.');
+    }
+
+    public function __get($name)
+    {
+        return $this->{$name};
+    }
+}
---
+++ tests/OpenCFP/Domain/AirportInfoTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace OpenCFP\Test\Domain;
+
+use OpenCFP\Domain\AirportInfo;
+
+class AirportInfoTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @test */
+    public function it_has_an_iata_location_identifier_name_and_country()
+    {
+        $airport = AirportInfo::make('RDU', 'Raleigh/Durham (NC)', 'USA');
+
+        $this->assertEquals('RDU', $airport->code);
+        $this->assertEquals('Raleigh/Durham (NC)', $airport->name);
+        $this->assertEquals('USA', $airport->country);
+    }
+
+    /** @test */
+    public function it_can_be_constructed_from_database_row()
+    {
+        $airport = AirportInfo::fromData([
+            'code' => 'RDU',
+            'name' => 'Raleigh/Durham (NC)',
+            'country' => 'USA',
+        ]);
+
+        $this->assertEquals('RDU', $airport->code);
+        $this->assertEquals('Raleigh/Durham (NC)', $airport->name);
+        $this->assertEquals('USA', $airport->country);
+    }
+
+}
