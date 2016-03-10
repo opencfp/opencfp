33cc284e05cba9fbe0ef0b80fd01c92d4b8b4416
Andreas Möller <am@localheinz.com>
2016-03-09 22:51:42 -0500

    Fix: Mock query and mapper

--- tests/Http/Controller/Admin/TalksControllerTest.php
+++ tests/Http/Controller/Admin/TalksControllerTest.php
@@ -4,7 +4,9 @@ namespace OpenCFP\Test\Http\Controller\Admin;
 
 use Mockery as m;
 use OpenCFP\Application;
+use OpenCFP\Domain\Entity\Mapper;
 use OpenCFP\Environment;
+use Spot\Query;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
 
@@ -195,11 +197,21 @@ class TalksControllerTest extends \PHPUnit_Framework_TestCase
     {
         $talkId = uniqid();
 
-        // Override our mapper with the double
+        $query = m::mock(Query::class);
+        $query->shouldReceive('with')->with(['comments'])->andReturnSelf();
+        $query->shouldReceive('first')->andReturnNull();
+
+        $talkMapper = m::mock(Mapper\Talk::class);
+        $talkMapper->shouldReceive('where')->with(['id' => $talkId])->andReturn($query);
+
+        $talkMetaMapper = m::mock(\Spot\Mapper::class);
+
         $spot = m::mock('Spot\Locator');
         $spot->shouldReceive('mapper')
             ->with(\OpenCFP\Domain\Entity\Talk::class)
-            ->andReturn([]);
+            ->andReturn($talkMapper);
+        $spot->shouldReceive('mapper')->with(\OpenCFP\Domain\Entity\TalkMeta::class)->andReturn($talkMetaMapper);
+
         $this->app['spot'] = $spot;
 
         // Create a session object
