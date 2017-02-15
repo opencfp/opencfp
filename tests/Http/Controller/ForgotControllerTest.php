9b58bb063cd13bc15b891651a8a1d3367460c2f7
Chris Hartjes <chartjes@littlehart.net>
2017-02-15 09:24:49 -0500

    Added more tests for ForgotController, created some helper methods for testing purposes

--- tests/Http/Controller/ForgotControllerTest.php
+++ tests/Http/Controller/ForgotControllerTest.php
@@ -51,14 +51,17 @@ class ForgotControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function sendResetDisplaysCorrectMessage()
     {
+        unset($this->app['sentry']);
+        $this->app['sentry'] = $this->createSentry($this->createUser());
+
+        // Override our reset_emailer service
+        $reset_emailer = m::mock('OpenCFP\Provider\ResetEmailerServiceProvider');
+        $reset_emailer->shouldReceive('send')->andReturn(true);
+        $this->app['reset_emailer'] = $reset_emailer;
+
         // We need to create a replacement form.factory to return a form we control
-        $form = m::mock('OpenCFP\Http\Form\ForgotForm');
-        $form->shouldReceive('handleRequest');
-        $form->shouldReceive('isValid')->andReturn(true);
-        $data = ['email' => 'test@opencfp.org'];
-        $form->shouldReceive('getData')->andReturn($data);
         $form_factory = m::mock('Silex\Provider\FormServiceProvider');
-        $form_factory->shouldReceive('createBuilder->getForm')->andReturn($form);
+        $form_factory->shouldReceive('createBuilder->getForm')->andReturn($this->createForm('valid'));
         $this->app['form.factory'] = $form_factory;
 
         $req = m::mock('Symfony\Component\HttpFoundation\Request');
@@ -79,11 +82,8 @@ class ForgotControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function invalidResetFormTriggersErrorMessage()
     {
-        $form = m::mock('OpenCFP\Http\Form\ForgotForm');
-        $form->shouldReceive('handleRequest');
-        $form->shouldReceive('isValid')->andReturn(false);
         $form_factory = m::mock('Silex\Provider\FormServiceProvider');
-        $form_factory->shouldReceive('createBuilder->getForm')->andReturn($form);
+        $form_factory->shouldReceive('createBuilder->getForm')->andReturn($this->createForm('not valid'));
         $this->app['form.factory'] = $form_factory;
 
         $req = m::mock('Symfony\Component\HttpFoundation\Request');
@@ -97,6 +97,84 @@ class ForgotControllerTest extends \PHPUnit_Framework_TestCase
             $flash_message['ext']
         );
 
+    }
+
+    /**
+     * @test
+     * @expectedException \Cartalyst\Sentry\Users\UserNotFoundException;
+     */
+    public function resetPasswordNotFindingUserCorrectlyThrowsException()
+    {
+        $form_factory = m::mock('Silex\Provider\FormServiceProvider');
+        $form_factory->shouldReceive('createBuilder->getForm')->andReturn($this->createForm('valid'));
+        $this->app['form.factory'] = $form_factory;
+
+        $req = m::mock('Symfony\Component\HttpFoundation\Request');
+        $controller = new \OpenCFP\Http\Controller\ForgotController();
+        $controller->setApplication($this->app);
+        $controller->sendResetAction($req);
+    }
+
+    /**
+     * @test
+     */
+    public function resetPasswordHandlesNotSendingResetEmailCorrectly()
+    {
+        unset($this->app['sentry']);
+        $this->app['sentry'] = $this->createSentry($this->createUser());
+
+        // Override our reset_emailer service
+        $reset_emailer = m::mock('OpenCFP\Provider\ResetEmailerServiceProvider');
+        $reset_emailer->shouldReceive('send')->andReturn(false);
+        $this->app['reset_emailer'] = $reset_emailer;
+
+        // We need to create a replacement form.factory to return a form we control
+        $form_factory = m::mock('Silex\Provider\FormServiceProvider');
+        $form_factory->shouldReceive('createBuilder->getForm')->andReturn($this->createForm('valid'));
+        $this->app['form.factory'] = $form_factory;
+
+        $req = m::mock('Symfony\Component\HttpFoundation\Request');
+        $controller = new \OpenCFP\Http\Controller\ForgotController();
+        $controller->setApplication($this->app);
+        $controller->sendResetAction($req);
+
+        // As long as the email validates as being a potential email, the flash message should indicate success
+        $flash_message = $this->app['session']->get('flash');
+        $this->assertContains(
+            'We were unable to send your password reset request. Please try again',
+            $flash_message['ext']
+        );
+    }
+
+    private function createUser()
+    {
+        $user = m::mock('StdClass');
+        $user->shouldReceive('getResetPasswordCode');
+        $user->shouldReceive('getId');
+
+        return $user;
+    }
+
+    private function createSentry($user)
+    {
+        $sentry = m::mock('Cartalyst\Sentry\Sentry');
+        $sentry->shouldReceive('getUserProvider->findByLogin')->andReturn($user);
+        $sentry->shouldReceive('check')->andReturn(true);
+        $sentry->shouldReceive('getUser')->andReturn($user);
+
+        return $sentry;
+    }
+
+    private function createForm($valid_status)
+    {
+        $is_valid = ($valid_status == 'valid');
+        $form = m::mock('OpenCFP\Http\Form\ForgotForm');
+        $form->shouldReceive('handleRequest');
+        $form->shouldReceive('isValid')->andReturn($is_valid);
+        $data = ['email' => 'test@opencfp.org'];
+        $form->shouldReceive('getData')->andReturn($data);
+
+        return $form;
     }
 }
 
