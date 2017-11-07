5e68891041302af90cce9f77a2e19997c46a62cd
BackEndTea <gdpagter@ibuildings.nl>
2017-11-07 15:03:00 +0100

    Add the skeleton for the reviewer controllers
    The controllers and functions exist, but since they have no return
    value they still throw a 500 error

---
+++ classes/Http/Controller/Reviewer/SpeakersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OpenCFP\Http\Controller\Reviewer;
+
+use OpenCFP\Http\Controller\BaseController;
+
+class SpeakersController extends BaseController
+{
+    use ReviewerAccessTrait;
+
+    public function indexAction()
+    {
+        //TODO: add function
+    }
+
+    public function viewAction()
+    {
+        //TODO: add function
+    }
+}
---
+++ classes/Http/Controller/Reviewer/TalksController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OpenCFP\Http\Controller\Reviewer;
+
+use OpenCFP\Http\Controller\BaseController;
+
+class TalksController extends BaseController
+{
+    use ReviewerAccessTrait;
+
+    public function indexAction()
+    {
+        //TODO: Add function
+    }
+
+    public function viewAction()
+    {
+        //TODO: add function
+    }
+
+    public function favoriteAction()
+    {
+        //TODO: add function
+    }
+
+    public function commentCreateAction()
+    {
+        //TODO: add function
+    }
+
+    public function rateAction()
+    {
+        //TODO: add function
+    }
+}
