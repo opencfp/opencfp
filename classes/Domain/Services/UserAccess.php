67b1a4b5f66299e64e22d9c5ebe515d0cd034f7e
BackEndTea <gdpagter@ibuildings.nl>
2017-11-10 09:09:10 +0100

    Run make cs

--- classes/Domain/Services/UserAccess.php
+++ classes/Domain/Services/UserAccess.php
@@ -11,8 +11,8 @@ interface UserAccess
      * If a user doesn't have access to a page they get redirected, otherwise nothing happens
      *
      * @param Application $app
-     *
      * @param string $role Role to check against
+     *
      * @return RedirectResponse|void
      */
     public static function userHasAccess(Application $app, $role = '');
