8786830a66ce2f42f3ad273d1d8e05aa939237c5
Dustin Wheeler <mdwheele@ncsu.edu>
2017-11-12 16:55:04 +0000

    Remove dependency on ircmaxell/random-lib and use random_bytes instead

--- classes/Infrastructure/Crypto/PseudoRandomStringGenerator.php
+++ classes/Infrastructure/Crypto/PseudoRandomStringGenerator.php
@@ -3,27 +3,11 @@
 namespace OpenCFP\Infrastructure\Crypto;
 
 use OpenCFP\Domain\Services\RandomStringGenerator;
-use RandomLib\Factory;
 
 class PseudoRandomStringGenerator implements RandomStringGenerator
 {
-    /**
-     * @var Generator
-     */
-    private $generator;
-
-    public function __construct(Factory $factory)
-    {
-        $this->generator = $factory->getMediumStrengthGenerator();
-    }
-
     public function generate($length = 40)
     {
-        return $this->generator->generateString($length, $this->getCharacterSet());
-    }
-
-    private function getCharacterSet()
-    {
-        return '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return substr(bin2hex(random_bytes($length)), 0, $length);
     }
 }
