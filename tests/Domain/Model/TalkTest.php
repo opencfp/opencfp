faf5a7394a8a215e6f592304c206b57070cbfb0e
BackEndTea <gdpagter@ibuildings.nl>
2017-11-14 12:00:42 +0100

    Update and increase Talk tests

--- tests/Domain/Model/TalkTest.php
+++ tests/Domain/Model/TalkTest.php
@@ -2,6 +2,7 @@
 
 namespace OpenCFP\Test\Domain\Model;
 
+use OpenCFP\Domain\Model\Favorite;
 use OpenCFP\Domain\Model\Talk;
 use OpenCFP\Domain\Model\TalkMeta;
 use OpenCFP\Test\BaseTestCase;
@@ -14,14 +15,10 @@ class TalkTest extends BaseTestCase
 {
     use RefreshDatabase;
 
-    private static $talks;
-
     public static function setUpBeforeClass()
     {
         parent::setUpBeforeClass();
-        $talks = factory(Talk::class, 2)->create(['selected' => 1]);
-        factory(TalkMeta::class, 1)->create(['admin_user_id' => 2, 'talk_id' => $talks->first()->id]);
-        self::$talks = $talks;
+        self::setUpTalksForTests();
     }
 
     /** @test */
@@ -37,7 +34,8 @@ class TalkTest extends BaseTestCase
     public function selectedOnlyReturnsSelectedTalks()
     {
         $selected = Talk::selected()->get();
-        $this->assertCount(2, $selected);
+        $this->assertCount(1, $selected);
+        $this->assertEquals('talks title NO 2', $selected->first()->title);
     }
 
     /**
@@ -45,9 +43,140 @@ class TalkTest extends BaseTestCase
      */
     public function viewedByOnlyReturnsViewedTalks()
     {
-        $viewedBy = Talk::viewedBy(2)->get();
-        $this->assertCount(1, $viewedBy);
-        $viewedByOther = Talk::viewedBy(5)->get();
+        $viewedBy = Talk::viewedBy(1)->get();
+        $this->assertCount(2, $viewedBy);
+
+        $viewedByOther = Talk::viewedBy(25)->get();
         $this->assertCount(0, $viewedByOther);
+    }
+
+    /**
+     * @test
+     */
+    public function favoritedByOnlyReturnsFavoritedTalks()
+    {
+        $favorited = Talk::favoritedBy(1)->get();
+
+        $this->assertCount(1, $favorited);
+        $this->assertEquals('talks title', $favorited->first()->title);
+
+        $favoritedByOther = Talk::viewedBy(25)->get();
+        $this->assertCount(0, $favoritedByOther);
+    }
+
+    /**
+     * @test
+     */
+    public function ratedPlusOneByReturnsPlusOneRatedTalks()
+    {
+        $ratedPlusOne = Talk::ratedPlusOneBy(1)->get();
+
+        $this->assertCount(1, $ratedPlusOne);
+        $this->assertEquals('talks title NO 2', $ratedPlusOne->first()->title);
+
+        $ratedPlusOneByOther = Talk::ratedPlusOneBy(25)->get();
+        $this->assertCount(0, $ratedPlusOneByOther);
+    }
+
+    /**
+     * @test
+     */
+    public function notRatedByReturnsTalksNotRatedOrRatedZero()
+    {
+        $notRated = Talk::notRatedBy(1)->get();
+        $this->assertCount(2, $notRated);
+
+        $notRatedByUserTwo = Talk::notRatedBy(25)->get();
+        $this->assertCount(3, $notRatedByUserTwo);
+    }
+
+    /**
+     * @test
+     */
+    public function notViewedByReturnsTalksNotViewed()
+    {
+        $notViewed = Talk::notViewedBy(1)->get();
+
+        $this->assertCount(1, $notViewed);
+        $this->assertEquals('talks title NO 3', $notViewed->first()->title);
+
+        $notViewedByOther = Talk::notViewedBy(2)->get();
+
+        $this->assertCount(3, $notViewedByOther);
+    }
+
+    /**
+     * @test
+     */
+    public function topRatedSortsOnBestRatings()
+    {
+        $topRated = Talk::topRated()->get();
+        $this->assertEquals('talks title NO 2', $topRated->first()->title);
+        $this->assertCount(2, $topRated);
+    }
+
+
+    protected static function setUpTalksForTests()
+    {
+        $talk = Talk::create([
+            'user_id' => 7,
+            'title' => 'talks title',
+            'description' => 'Long description',
+            'type' => '',
+            'level' => 'entry',
+            'category' => 'api',
+            'selected' => 0,
+        ]);
+
+        $talkTwo =Talk::create([
+            'user_id' => 7,
+            'title' => 'talks title NO 2',
+            'description' => 'Long description',
+            'type' => 'regular',
+            'level' => 'entry',
+            'category' => 'api',
+            'selected' => 1,
+
+        ]);
+
+        Talk::create([
+            'user_id' => 7,
+            'title' => 'talks title NO 3',
+            'description' => 'Long description',
+            'type' => 'regular',
+            'level' => 'entry',
+            'category' => 'api',
+            'selected' => 0,
+        ]);
+
+        TalkMeta::create([
+            'admin_user_id' => 1,
+            'talk_id' => $talk->id,
+            'rating' => 0,
+            'viewed' => 1,
+        ]);
+
+        TalkMeta::create([
+            'admin_user_id' => 1,
+            'talk_id' => $talkTwo->id,
+            'rating' => 1,
+            'viewed' => 1,
+        ]);
+        TalkMeta::create([
+            'admin_user_id' => 2,
+            'talk_id' => $talkTwo->id,
+            'rating' => 1,
+            'viewed' => 0,
+        ]);
+        TalkMeta::create([
+            'admin_user_id' => 8,
+            'talk_id' => $talk->id,
+            'rating' => 1,
+            'viewed' => 0,
+        ]);
+        Favorite::create([
+            'admin_user_id' => 1,
+            'talk_id' => $talk->id,
+        ]);
     }
 }
