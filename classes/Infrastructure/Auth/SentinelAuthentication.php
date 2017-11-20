84e13b8ae269ed67eedf0a74349527c77348d543
BackEndTea <gdpagter@ibuildings.nl>
2017-11-20 16:29:17 +0100

    Add a sentinel version of Authentication
    Added a sentinel version of the authentication class, with tests

---
+++ classes/Infrastructure/Auth/SentinelAuthentication.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace OpenCFP\Infrastructure\Auth;
+
+use Cartalyst\Sentinel\Sentinel;
+use OpenCFP\Domain\Services\Authentication;
+use OpenCFP\Domain\Services\AuthenticationException;
+use OpenCFP\Domain\Services\NotAuthenticatedException;
+
+class SentinelAuthentication implements Authentication
+{
+    /**
+     * @var Sentinel
+     */
+    private $sentinel;
+
+    /**
+     * @var SentinelAccountManagement
+     */
+    private $accountManagement;
+
+    public function __construct(Sentinel $sentinel, SentinelAccountManagement $accountManagement)
+    {
+        $this->sentinel          = $sentinel;
+        $this->accountManagement = $accountManagement;
+    }
+
+    /**
+     * Given valid credentials, authenticate the user.
+     *
+     * @param string $username
+     * @param string $password
+     *
+     * @throws AuthenticationException
+     */
+    public function authenticate($username, $password)
+    {
+        try {
+            $success = false;
+            $user    = $this->accountManagement->findByLogin($username);
+            if ($user->checkPassword($password)) {
+                $success = $this->sentinel->login($user->getUser());
+            }
+            if (!$success) {
+                throw new AuthenticationException('Failure to login.');
+            }
+        } catch (\Throwable $e) {
+            throw new AuthenticationException('Failure to login.');
+        }
+    }
+
+    /**
+     * Returns current authenticated User account.
+     *
+     * @return UserInterface
+     *
+     * @throws NotAuthenticatedException
+     */
+    public function user(): UserInterface
+    {
+        $user = $this->sentinel->getUser();
+
+        return new SentinelUser($user);
+    }
+
+    /**
+     * Returns current authenticated User Id.
+     *
+     * @return int
+     *
+     * @throws NotAuthenticatedException
+     */
+    public function userId(): int
+    {
+        return $this->sentinel->getUser()->getUserId();
+    }
+
+    /**
+     * Determines whether or not the user is logged in.
+     *
+     * @return bool
+     */
+    public function check(): bool
+    {
+        return $this->sentinel->check() !== false;
+    }
+
+    /**
+     * Determine whether the user is a non-authenticated guest.
+     *
+     * @return bool
+     */
+    public function guest(): bool
+    {
+        return !$this->sentinel->check();
+    }
+
+    /**
+     * Destroys the user's active authenticated session.
+     */
+    public function logout()
+    {
+        return $this->sentinel->logout();
+    }
+}
---
+++ tests/Infrastructure/Auth/SentinelAuthenticationTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace OpenCFP\Test\Infrastructure\Auth;
+
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use OpenCFP\Domain\Services\AuthenticationException;
+use OpenCFP\Infrastructure\Auth\SentinelAccountManagement;
+use OpenCFP\Infrastructure\Auth\SentinelAuthentication;
+use OpenCFP\Test\BaseTestCase;
+use OpenCFP\Test\DataBaseInteraction;
+
+/**
+ * @covers \OpenCFP\Infrastructure\Auth\SentinelAuthentication
+ */
+class SentinelAuthenticationTest extends BaseTestCase
+{
+    use DataBaseInteraction;
+    /**
+     * @var SentinelAuthentication
+     */
+    private $sut;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $sentinel = (new Sentinel())->getSentinel();
+        $accounts = new SentinelAccountManagement($sentinel);
+        $accounts->create('test@example.com', 'secret');
+        $accounts->activate('test@example.com');
+        $this->sut = new SentinelAuthentication($sentinel, $accounts);
+    }
+
+    /**
+     * @test
+     */
+    public function existing_user_can_authenticate()
+    {
+        $this->sut->authenticate('test@example.com', 'secret');
+        $this->assertTrue($this->sut->check());
+
+        $user = $this->sut->user();
+
+        $this->assertEquals('test@example.com', $user->getLogin());
+    }
+
+    /**
+     * @test
+     */
+    public function wrongUserCanNotAuthenticate()
+    {
+        $this->expectException(AuthenticationException::class);
+        $this->sut->authenticate('wrong@user.com', 'secret');
+    }
+
+    /**
+     * @test
+     */
+    public function wrongPasswordCantAuthenticate()
+    {
+        $this->expectException(AuthenticationException::class);
+        $this->sut->authenticate('test@example.com', 'Secret');
+    }
+
+    /**
+     * @test
+     */
+    public function userIdWorks()
+    {
+        $this->sut->authenticate('test@example.com', 'secret');
+        $this->assertSame(1, $this->sut->userId());
+    }
+
+    /**
+     * @test
+     */
+    public function checkWorks()
+    {
+        $this->assertFalse($this->sut->check());
+        $this->sut->authenticate('test@example.com', 'secret');
+        $this->assertTrue($this->sut->check());
+    }
+
+    /**
+     * @test
+     */
+    public function guestWorks()
+    {
+        $this->assertTrue($this->sut->guest());
+        $this->sut->authenticate('test@example.com', 'secret');
+        $this->assertFalse($this->sut->guest());
+    }
+}
