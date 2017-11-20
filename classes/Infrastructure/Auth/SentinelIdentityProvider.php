047e821ed5dbcc8fe647bd6f7281cd43dc618f6f
BackEndTea <gdpagter@ibuildings.nl>
2017-11-20 16:38:09 +0100

    Add a sentinel version of IdentityProvider
    Added a sentinel version of the IdentityProvider class, with tests

---
+++ classes/Infrastructure/Auth/SentinelIdentityProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OpenCFP\Infrastructure\Auth;
+
+use Cartalyst\Sentinel\Sentinel;
+use OpenCFP\Domain\Model\User;
+use OpenCFP\Domain\Services\IdentityProvider;
+use OpenCFP\Domain\Services\NotAuthenticatedException;
+use OpenCFP\Domain\Speaker\SpeakerRepository;
+
+class SentinelIdentityProvider implements IdentityProvider
+{
+    private $sentinel;
+    private $repo;
+
+    public function __construct(Sentinel $sentinel, SpeakerRepository $repo)
+    {
+        $this->sentinel = $sentinel;
+        $this->repo     = $repo;
+    }
+
+    /**
+     * Retrieves the currently authenticate user's username.
+     *
+     * @return User
+     *
+     * @throws NotAuthenticatedException
+     */
+    public function getCurrentUser()
+    {
+        $user = $this->sentinel->getUser();
+        if ($user == null || !$user) {
+            throw new NotAuthenticatedException();
+        }
+
+        return $this->repo->findById($user->getUserId());
+    }
+}
