b1e4018bb4a0a367034b7fb1d5a142e4ea658852
BackEndTea <gdpagter@ibuildings.nl>
2017-11-20 16:14:52 +0100

    Add a sentinel version of the account management and the user
    These new versions of the account management and the user are here
    to replace their old sentry versions.
    
    Some of the functionality isn't very 'pretty' so to say, since the way
    some things are handled in Sentinel are a bit different from Sentry.
    
    The current version should be seen as an in-between, to make the change
    from Sentry to Sentinel.

---
+++ tests/Infrastructure/Auth/SentinelUserTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace OpenCFP\Test\Infrastructure\Auth;
+
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use OpenCFP\Infrastructure\Auth\SentinelAccountManagement;
+use OpenCFP\Infrastructure\Auth\SentinelUser;
+use OpenCFP\Infrastructure\Auth\UserInterface;
+use OpenCFP\Test\BaseTestCase;
+use OpenCFP\Test\RefreshDatabase;
+
+/**
+ * @covers \OpenCFP\Infrastructure\Auth\SentinelUser
+ */
+class SentinelUserTest extends BaseTestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @var SentinelUser
+     */
+    private static $user;
+
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+        $account = new SentinelAccountManagement((new Sentinel())->getSentinel());
+        $account->create('test@example.com', 'secret', [
+            'first_name' => 'Test',
+            'last_name'  => 'Account',
+        ]);
+        self::$user = $account->findByLogin('test@example.com');
+        $account->promoteTo('test@example.com', 'Speaker');
+    }
+
+    /**
+     * @test
+     */
+    public function weHaveTheRightUser()
+    {
+        $this->assertInstanceOf(UserInterface::class, self::$user);
+    }
+
+    /**
+     * @test
+     */
+    public function getIdWorks()
+    {
+        $this->assertSame(1, self::$user->getId());
+    }
+
+    /**
+     * @test
+     */
+    public function getLoginWorks()
+    {
+        $this->assertSame('test@example.com', self::$user->getLogin());
+    }
+
+    /**
+     * @test
+     */
+    public function hasAccessWorks()
+    {
+        $user = self::$user;
+        $this->assertSame(false, $user->hasAccess('Admin'));
+        $this->assertSame(false, $user->hasAccess('Reviewer'));
+        $this->assertSame(true, $user->hasAccess('Speaker'));
+        $this->assertSame(false, $user->hasAccess('NotExistingThing'));
+    }
+
+    /**
+     * @test
+     */
+    public function checkPasswordWorks()
+    {
+        $this->assertTrue(self::$user->checkPassword('secret'));
+    }
+
+    /**
+     * @test
+     */
+    public function checkResetPasswordCodeWorks()
+    {
+        /** @var Capsule $capsule */
+        $capsule = $this->app[Capsule::class];
+        $capsule->getConnection()->query()->from('reminders')->insert([
+            'user_id' => self::$user->getId(),
+            'code'    => 'secret.reset.code',
+        ]);
+        $this->assertFalse(self::$user->checkResetPasswordCode('wrong.code'));
+        $this->assertTrue(self::$user->checkResetPasswordCode('secret.reset.code'));
+        //reset after test.
+        $capsule->getConnection()->query()->from('reminders')->truncate();
+    }
+
+    /**
+     * @test
+     */
+    public function getResetPassWordCodeWorks()
+    {
+        /** @var Capsule $capsule */
+        $capsule = $this->app[Capsule::class];
+        $capsule->getConnection()->query()->from('reminders')->insert([
+            'user_id' => self::$user->getId(),
+            'code'    => 'secret.reset.code',
+        ]);
+        $this->assertTrue(self::$user->checkResetPasswordCode('secret.reset.code'));
+
+        // The function resets the password code when it requests it.
+        $this->assertNotSame('secret.reset.code', self::$user->getResetPasswordCode());
+        $capsule->getConnection()->query()->from('reminders')->truncate();
+    }
+
+    /**
+     * @test
+     */
+    public function attemptResetPasswordWorks()
+    {
+        /** @var Capsule $capsule */
+        $capsule = $this->app[Capsule::class];
+        $capsule->getConnection()->query()->from('reminders')->insert([
+            'user_id' => self::$user->getId(),
+            'code'    => 'secret.reset.code',
+        ]);
+
+        $result = self::$user->attemptResetPassword('wrong code', 'newPass1');
+        $this->assertFalse($result);
+        $secondTry = self::$user->attemptResetPassword('secret.reset.code', 'newPass2');
+        $this->assertTrue($secondTry);
+        $this->assertTrue(self::$user->checkPassword('newPass2'));
+    }
+
+    /**
+     * @test
+     */
+    public function getUserWorks()
+    {
+        $user = self::$user->getUser();
+
+        $this->assertInstanceOf(\Cartalyst\Sentinel\Users\UserInterface::class, $user);
+    }
+}
