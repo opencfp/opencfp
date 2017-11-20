6d0b1f4aaf2437380a1ab7ece115b5a80a13d51c
BackEndTea <gdpagter@ibuildings.nl>
2017-11-20 15:31:01 +0100

    Add a new dump.sql file for the tests to work with

--- tests/dump.sql
+++ tests/dump.sql
@@ -7,6 +7,21 @@
 DROP DATABASE IF EXISTS`cfp_test` ;
 CREATE DATABASE `cfp_test`;
 USE `cfp_test`;
+--
+-- Table structure for table `activations`
+--
+
+DROP TABLE IF EXISTS `activations`;
+CREATE TABLE `activations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `completed_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `airports`
@@ -64,9 +79,82 @@ CREATE TABLE `groups` (
 
 LOCK TABLES `groups` WRITE;
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES (1,'Speakers','{\"users\":1}','2017-11-13 14:54:21','2017-11-13 14:54:21'),(2,'Admin','{\"admin\":1}','2017-11-13 14:54:21','2017-11-13 14:54:21'),(3,'Reviewer','{\"reviewer\":1}','2017-11-13 14:54:22','2017-11-13 14:54:22');
+INSERT INTO `groups` VALUES (1,'Speakers','{\"users\":1}','2017-11-20 14:56:22','2017-11-20 14:56:22'),(2,'Admin','{\"admin\":1}','2017-11-20 14:56:22','2017-11-20 14:56:22'),(3,'Reviewer','{\"reviewer\":1}','2017-11-20 14:56:23','2017-11-20 14:56:23');
 /*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `persistences`
+--
+
+DROP TABLE IF EXISTS `persistences`;
+CREATE TABLE `persistences` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `reminders`
+--
+
+DROP TABLE IF EXISTS `reminders`;
+CREATE TABLE `reminders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `completed_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `role_users`
+--
+
+DROP TABLE IF EXISTS `role_users`;
+CREATE TABLE `role_users` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `slug` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `permissions` text,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `roles_slug_unique` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'speaker','Speaker','{\"talk.update\":true,\"talk.review\":false,\"user.delete\":false}','2017-11-20 13:56:24',NULL),(2,'reviewer','Reviewer','{\"talk.update\":true,\"talk.review\":true,\"user.delete\":false}','2017-11-20 13:56:24',NULL),(3,'admin','Admin','{\"talk.update\":true,\"talk.review\":true,\"user.delete\":true}','2017-11-20 13:56:24',NULL);
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 --
 -- Table structure for table `talk_comments`
@@ -169,4 +257,4 @@ CREATE TABLE `users_groups` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dump completed on 2017-11-13 14:54:36
+-- Dump completed on 2017-11-20 15:03:18
