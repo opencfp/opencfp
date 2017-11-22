cc7d51b6cf97e5f7753edd09a81097f1535e4fb2
Andreas Möller <am@localheinz.com>
2017-11-22 23:17:48 +0100

    Fix: Use return type declarations

--- classes/Domain/Speaker/NotAllowedException.php
+++ classes/Domain/Speaker/NotAllowedException.php
@@ -4,7 +4,7 @@ namespace OpenCFP\Domain\Speaker;
 
 class NotAllowedException extends \Exception
 {
-    public static function notAllowedToView(string $property)
+    public static function notAllowedToView(string $property): self
     {
         return new self(sprintf('Not allowed to view %s. Hidden property', $property));
     }
