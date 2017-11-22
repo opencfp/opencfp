7fd0139bea19568c5c817cdf435e74861b088f0d
Andreas Möller <am@localheinz.com>
2017-11-22 23:44:59 +0100

    Fix: Run 'make cs'

--- web/index_dev.php
+++ web/index_dev.php
@@ -1,7 +1,7 @@
 <?php
 
 $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
-if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+if (PHP_SAPI === 'cli-server' && is_file($filename)) {
     return false;
 }
 
