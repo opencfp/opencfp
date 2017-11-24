61a4a59e1535149a27cd855504357587b5b3a789
BackEndTea <gdpagter@ibuildings.nl>
2017-11-24 09:04:52 +0100

    Improve talk rating tests
     * Added two new test classed
     * Split up tests
     * Added dataproviders to test a lot more options

---
+++ tests/Unit/Domain/Services/TalkRating/TalkRatingContextTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace OpenCFP\Test\Unit\Domain\Services\TalkRating;
+
+use Mockery;
+use OpenCFP\Domain\Services\Authentication;
+use OpenCFP\Domain\Services\TalkRating\TalkRatingContext;
+use OpenCFP\Domain\Services\TalkRating\TalkRatingStrategy;
+use OpenCFP\Domain\Services\TalkRating\YesNoRating;
+
+/**
+ * @covers \OpenCFP\Domain\Services\TalkRating\TalkRatingContext
+ */
+class TalkRatingContextTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     */
+    public function getTalkStrategyReturnsATalkRatingStrategy()
+    {
+        $strategy = TalkRatingContext::getTalkStrategy('yesno', $this->authMock());
+        $this->assertInstanceOf(TalkRatingStrategy::class, $strategy);
+    }
+
+    /**
+     * @test
+     */
+    public function getTalkStrategyWithYesNoReturnsYesNoRating()
+    {
+        $strategy = TalkRatingContext::getTalkStrategy('yesno', $this->authMock());
+        $this->assertInstanceOf(YesNoRating::class, $strategy);
+    }
+
+    /**
+     * @test
+     */
+    public function casingDoesNotMatterForGetTalkRatingStrategy()
+    {
+        $strategy = TalkRatingContext::getTalkStrategy('YeSNo', $this->authMock());
+        $this->assertInstanceOf(YesNoRating::class, $strategy);
+    }
+
+    /**
+     * @dataProvider strategyProvider
+     *
+     * @test
+     */
+    public function defaultStrategyIsYesNoRating($input)
+    {
+        $strategy = TalkRatingContext::getTalkStrategy($input, $this->authMock());
+        $this->assertInstanceOf(YesNoRating::class, $strategy);
+    }
+
+    public function strategyProvider(): array
+    {
+        return [
+            [1],
+            ['asdf'],
+            [''],
+            ['NULL'],
+            [false],
+            [true],
+        ];
+    }
+
+    /**
+     * @return Authentication|Mockery\MockInterface
+     */
+    public function authMock()
+    {
+        $auth = Mockery::mock(Authentication::class);
+        $auth->shouldReceive('userId');
+
+        return $auth;
+    }
+}
