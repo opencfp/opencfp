b3bbb30aaa4538bf44dba732ba3466cd4b570745
Andreas Möller <am@localheinz.com>
2017-11-25 11:33:49 +0100

    Fix: Run 'make cs'

--- tests/helpers.php
+++ tests/helpers.php
@@ -19,7 +19,8 @@ if (!function_exists('factory')) {
         $arguments = func_get_args();
         if (isset($arguments[1]) && is_string($arguments[1])) {
             return $factory->of($arguments[0], $arguments[1])->times($arguments[2] ?? null);
-        } elseif (isset($arguments[1])) {
+        }
+        if (isset($arguments[1])) {
             return $factory->of($arguments[0])->times($arguments[1]);
         }
 
