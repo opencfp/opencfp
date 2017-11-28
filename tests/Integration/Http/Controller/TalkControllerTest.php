181e7c3db07c0b571019e0953b4f2ad8d0a837af
Andreas Möller <am@localheinz.com>
2017-11-28 18:18:19 +0100

    Fix: Run 'make cs'

--- tests/Integration/Http/Controller/TalkControllerTest.php
+++ tests/Integration/Http/Controller/TalkControllerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
 namespace OpenCFP\Test\Integration\Http\Controller;
 
 use Mockery as m;
