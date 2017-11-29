19be64070b75d571d6ca755d0ddaa43d90625dc6
BackEndTea <gdpagter@ibuildings.nl>
2017-11-29 16:03:54 +0100

    Remove yesno case as it is the default
    The case statement was useless, as it was the default as well.

--- classes/Domain/Services/TalkRating/TalkRatingContext.php
+++ classes/Domain/Services/TalkRating/TalkRatingContext.php
@@ -22,8 +22,6 @@ class TalkRatingContext
     {
         $strategy = \strtolower($strategy);
         switch ($strategy) {
-            case 'yesno':
-                return new YesNoRating(new TalkMeta(), $auth);
             case 'onetoten':
                 return new OneToTenRating(new TalkMeta(), $auth);
             default:
