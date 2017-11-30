7afc93c965de6af8ace3cd3769ebf8da9f78f4c6
Andreas Möller <am@localheinz.com>
2017-11-30 16:15:55 +0100

    Enhancement: Extract PathInterface

---
+++ classes/PathInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
+namespace OpenCFP;
+
+interface PathInterface
+{
+    public function basePath(): string;
+
+    public function configPath(): string;
+
+    public function uploadPath(): string;
+
+    public function templatesPath(): string;
+
+    public function publicPath(): string;
+
+    public function assetsPath(): string;
+
+    public function cacheTwigPath(): string;
+
+    public function cachePurifierPath(): string;
+}
