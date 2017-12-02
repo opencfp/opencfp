d1ea1e45539fb880c901dc265e4aabb10bc31bd7
Andreas Möller <am@localheinz.com>
2017-12-02 13:02:49 +0100

    Enhancement: Implement RoleNotFoundException

---
+++ tests/Unit/Infrastructure/Auth/RoleNotFoundExceptionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
+namespace OpenCFP\Test\Unit\Infrastructure\Auth;
+
+use OpenCFP\Infrastructure\Auth\RoleNotFoundException;
+use OpenCFP\Test\Helper\Faker\GeneratorTrait;
+
+/**
+ * @covers \OpenCFP\Infrastructure\Auth\RoleNotFoundException
+ */
+final class RoleNotFoundExceptionTest extends \PHPUnit\Framework\TestCase
+{
+    use GeneratorTrait;
+
+    public function testIsFinal()
+    {
+        $reflection = new \ReflectionClass(RoleNotFoundException::class);
+
+        $this->assertTrue($reflection->isFinal());
+    }
+
+    public function testIsRuntimeException()
+    {
+        $exception = new RoleNotFoundException();
+
+        $this->assertInstanceOf(\RuntimeException::class, $exception);
+    }
+
+    public function testFromNameReturnsException()
+    {
+        $name = $this->getFaker()->word;
+
+        $exception = RoleNotFoundException::fromName($name);
+
+        $this->assertInstanceOf(RoleNotFoundException::class, $exception);
+
+        $message = \sprintf(
+            'Unable to find a role with name "%s".',
+            $name
+        );
+
+        $this->assertSame($message, $exception->getMessage());
+        $this->assertSame(0, $exception->getCode());
+    }
+}
