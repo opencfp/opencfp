31180a452d149501680f3f4f613602b41fdb5f2c
Dustin Wheeler <mdwheele@ncsu.edu>
2017-12-15 12:25:16 +0000

    Fix CS

--- tests/Features/Contexts/FeatureContext.php
+++ tests/Features/Contexts/FeatureContext.php
@@ -1,10 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
 namespace OpenCFP\Test\Features\Contexts;
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
 
 /**
@@ -12,7 +21,6 @@ use Behat\MinkExtension\Context\MinkContext;
  */
 class FeatureContext extends MinkContext
 {
-
     public function __construct()
     {
     }
