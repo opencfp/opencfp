01ae741781e771e45e8a7348fe35c2c58850e439
BackEndTea <gdpagter@ibuildings.nl>
2017-12-15 10:31:04 +0100

    Move Pages Controller to Actions

---
+++ classes/Http/Action/Pages/HomePageAction.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
+namespace OpenCFP\Http\Action\Pages;
+
+use OpenCFP\Domain\Model\Talk;
+use Symfony\Component\HttpFoundation;
+use Twig_Environment;
+
+final class HomePageAction
+{
+    /**
+     * @var Twig_Environment
+     */
+    private $twig;
+
+    /**
+     * @var bool
+     */
+    private $showSubmissionCount;
+
+    public function __construct(Twig_Environment $twig, bool $showSubmissionCount)
+    {
+        $this->twig                = $twig;
+        $this->showSubmissionCount = $showSubmissionCount;
+    }
+
+    public function __invoke(): HttpFoundation\Response
+    {
+        $content = $this->twig->render('home.twig', [
+            'number_of_talks' => $this->showSubmissionCount ? Talk::count() : '',
+        ]);
+
+        return new HttpFoundation\Response($content);
+    }
+}
---
+++ tests/Unit/Http/Action/Pages/SpeakerPackageActionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
+namespace OpenCFP\Test\Unit\Http\Action\Pages;
+
+use Localheinz\Test\Util\Helper;
+use OpenCFP\Http\Action\Pages\SpeakerPackageAction;
+use PHPUnit\Framework;
+use Symfony\Component\HttpFoundation;
+use Twig_Environment;
+
+final class SpeakerPackageActionTest extends Framework\TestCase
+{
+    use Helper;
+
+    public function testItReturnsTheContentOfTheTwigInAResponseObject()
+    {
+        $content = $this->faker()->text();
+        $twig    = $this->createTwigMock();
+
+        $twig
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                $this->identicalTo('package.twig')
+            )
+            ->willReturn($content);
+        $action   = new SpeakerPackageAction($twig);
+        $response = $action();
+        $this->assertInstanceOf(HttpFoundation\Response::class, $response);
+        $this->assertContains($content, $response->getContent());
+        $this->assertSame(HttpFoundation\Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    /**
+     * @return Framework\MockObject\MockObject|Twig_Environment
+     */
+    private function createTwigMock(): Twig_Environment
+    {
+        return $this->createMock(Twig_Environment::class);
+    }
+}
