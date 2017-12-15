66767b55a02ba803534f1073576616875e1b6c42
Dustin Wheeler <mdwheele@ncsu.edu>
2017-12-15 06:26:52 -0500

    Run vendor/bin/behat --init and put feature files in tests directory

---
+++ tests/Features/Contexts/FeatureContext.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OpenCFP\Test\Features\Contexts;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext
+{
+
+    public function __construct()
+    {
+    }
+}
