8127f0eabd74eff654684d667d285cc72622c01f
Andreas Möller <am@localheinz.com>
2017-12-18 22:50:37 +0100

    Fix: Only use RefreshDatabase when needed

--- tests/Integration/Domain/Model/AirportTest.php
+++ tests/Integration/Domain/Model/AirportTest.php
@@ -15,13 +15,10 @@ namespace OpenCFP\Test\Integration\Domain\Model;
 
 use OpenCFP\Domain\EntityNotFoundException;
 use OpenCFP\Domain\Model\Airport;
-use OpenCFP\Test\Helper\RefreshDatabase;
 use OpenCFP\Test\Integration\WebTestCase;
 
 final class AirportTest extends WebTestCase
 {
-    use RefreshDatabase;
-
     /**
      * @var Airport
      */
