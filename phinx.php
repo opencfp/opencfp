b13de5ef4955cf68ede8053e71a3fe2e955fb338
Dustin Wheeler <mdwheele@ncsu.edu>
2017-12-20 04:27:50 +0000

    Phinx code-style is super strict

--- phinx.php
+++ phinx.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
 use OpenCFP\Kernel;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -15,13 +26,13 @@ return [
     ],
     'environments' => [
         'default_migration_table' => 'phinxlog',
-        'default_database' => $kernel->getEnvironment(),
+        'default_database'        => $kernel->getEnvironment(),
         $kernel->getEnvironment() => [
             'adapter' => 'mysql',
-            'host' => $container->getParameter('database.host'),
-            'name' => $container->getParameter('database.database'),
-            'user' => $container->getParameter('database.user'),
-            'pass' => $container->getParameter('database.password'),
+            'host'    => $container->getParameter('database.host'),
+            'name'    => $container->getParameter('database.database'),
+            'user'    => $container->getParameter('database.user'),
+            'pass'    => $container->getParameter('database.password'),
         ],
-    ]
+    ],
 ];
