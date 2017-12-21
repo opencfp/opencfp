9c33262a3e40dfe3aa3c146e46d298fd192a74e5
Andreas Möller <am@localheinz.com>
2017-12-21 19:34:27 +0100

    Enhancement: Extract ResetProcessAction from ForgotController

---
+++ classes/Http/Action/Forgot/ResetProcessAction.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
+namespace OpenCFP\Http\Action\Forgot;
+
+use OpenCFP\Domain\Services;
+use Symfony\Component\Form;
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\Routing;
+use Twig_Environment;
+
+final class ResetProcessAction
+{
+    /**
+     * @var Form\FormInterface
+     */
+    private $resetForm;
+
+    /**
+     * @var Services\AccountManagement
+     */
+    private $accountManagement;
+
+    /**
+     * @var Twig_Environment
+     */
+    private $twig;
+
+    /**
+     * @var Routing\Generator\UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    public function __construct(
+        Form\FormInterface $resetForm,
+        Services\AccountManagement $accountManagement,
+        Twig_Environment $twig,
+        Routing\Generator\UrlGeneratorInterface $urlGenerator
+    ) {
+        $this->resetForm         = $resetForm;
+        $this->accountManagement = $accountManagement;
+        $this->twig              = $twig;
+        $this->urlGenerator      = $urlGenerator;
+    }
+
+    public function __invoke(HttpFoundation\Request $request): HttpFoundation\Response
+    {
+        $userId    = $request->get('user_id');
+        $resetCode = $request->get('reset_code');
+
+        if (empty($resetCode)) {
+            throw new \Exception();
+        }
+
+        $this->resetForm->handleRequest($request);
+
+        if (!$this->resetForm->isSubmitted() || !$this->resetForm->isValid()) {
+            $this->resetForm->get('user_id')->setData($userId);
+            $this->resetForm->get('reset_code')->setData($resetCode);
+
+            $content = $this->twig->render('user/reset_password.twig', [
+                'form' => $this->resetForm->createView(),
+            ]);
+
+            return new HttpFoundation\Response($content);
+        }
+
+        try {
+            $user = $this->accountManagement->findById((int) $userId);
+        } catch (\RuntimeException $e) {
+            $request->getSession()->set('flash', [
+                'type'  => 'error',
+                'short' => 'Error',
+                'ext'   => 'The reset you have requested appears to be invalid, please try again.',
+            ]);
+
+            $url = $this->urlGenerator->generate('forgot_password');
+
+            return new HttpFoundation\RedirectResponse($url);
+        }
+
+        if (!$user->checkResetPasswordCode($resetCode)) {
+            $request->getSession()->set('flash', [
+                'type'  => 'error',
+                'short' => 'Error',
+                'ext'   => 'The reset you have requested appears to be invalid, please try again.',
+            ]);
+
+            $url = $this->urlGenerator->generate('forgot_password');
+
+            return new HttpFoundation\RedirectResponse($url);
+        }
+
+        $url = $this->urlGenerator->generate('forgot_password');
+
+        return new HttpFoundation\RedirectResponse($url);
+    }
+}
---
+++ tests/Unit/Http/Action/Forgot/ResetProcessActionTest.php
@@ -0,0 +1,508 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2017 OpenCFP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/opencfp/opencfp
+ */
+
+namespace OpenCFP\Test\Unit\Http\Action\Forgot;
+
+use Localheinz\Test\Util\Helper;
+use OpenCFP\Domain\Services;
+use OpenCFP\Http\Action\Forgot\ResetProcessAction;
+use OpenCFP\Infrastructure\Auth;
+use PHPUnit\Framework;
+use Prophecy\Argument;
+use Symfony\Component\Form;
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\Routing;
+use Twig_Environment;
+
+final class ResetProcessActionTest extends Framework\TestCase
+{
+    use Helper;
+
+    /**
+     * @dataProvider providerEmptyResetCode
+     *
+     * @param mixed $resetCode
+     */
+    public function testThrowsExceptionIfResetCodeIsEmpty($resetCode)
+    {
+        $userId = $this->faker()->numberBetween(1);
+
+        $request = $this->prophesize(HttpFoundation\Request::class);
+
+        $request
+            ->get(Argument::exact('user_id'))
+            ->shouldBeCalled()
+            ->willReturn((string) $userId);
+
+        $request
+            ->get(Argument::exact('reset_code'))
+            ->shouldBeCalled()
+            ->willReturn($resetCode);
+
+        $action = new ResetProcessAction(
+            $this->prophesize(Form\FormInterface::class)->reveal(),
+            $this->prophesize(Services\AccountManagement::class)->reveal(),
+            $this->prophesize(Twig_Environment::class)->reveal(),
+            $this->prophesize(Routing\Generator\UrlGeneratorInterface::class)->reveal()
+        );
+
+        $this->expectException(\Exception::class);
+
+        $action($request->reveal());
+    }
+
+    public function providerEmptyResetCode(): array
+    {
+        $values = [
+            'array-empty'  => [],
+            'bool-false'   => false,
+            'int-zero'     => 0,
+            'null'         => null,
+            'string-empty' => '',
+            'string-zero'  => '0',
+        ];
+
+        return \array_map(function ($value) {
+            return [
+                $value,
+            ];
+        }, $values);
+    }
+
+    public function testRendersFormIfFormIsNotSubmitted()
+    {
+        $faker = $this->faker();
+
+        $userId    = $faker->numberBetween(1);
+        $resetCode = $faker->sha256;
+
+        $content = $faker->text;
+
+        $request = $this->prophesize(HttpFoundation\Request::class);
+
+        $request
+            ->get(Argument::exact('user_id'))
+            ->shouldBeCalled()
+            ->willReturn((string) $userId);
+
+        $request
+            ->get(Argument::exact('reset_code'))
+            ->shouldBeCalled()
+            ->willReturn($resetCode);
+
+        $resetFormView = $this->prophesize(Form\FormView::class);
+
+        $userElement = $this->prophesize(Form\FormInterface::class);
+
+        $userElement
+            ->setData(Argument::exact((string) $userId))
+            ->shouldBeCalled();
+
+        $resetCodeElement = $this->prophesize(Form\FormInterface::class);
+
+        $resetCodeElement
+            ->setData(Argument::exact($resetCode))
+            ->shouldBeCalled();
+
+        $resetForm = $this->prophesize(Form\FormInterface::class);
+
+        $resetForm
+            ->handleRequest(Argument::exact($request))
+            ->shouldBeCalled();
+
+        $resetForm
+            ->isSubmitted()
+            ->shouldBeCalled()
+            ->willReturn(false);
+
+        $resetForm
+            ->get(Argument::exact('user_id'))
+            ->shouldBeCalled()
+            ->willReturn($userElement);
+
+        $resetForm
+            ->get(Argument::exact('reset_code'))
+            ->shouldBeCalled()
+            ->willReturn($resetCodeElement);
+
+        $resetForm
+            ->createView()
+            ->shouldBeCalled()
+            ->willReturn($resetFormView);
+
+        $twig = $this->prophesize(Twig_Environment::class);
+
+        $twig
+            ->render(
+                Argument::exact('user/reset_password.twig'),
+                Argument::exact([
+                    'form' => $resetFormView->reveal(),
+                ])
+            )
+            ->shouldBeCalled()
+            ->willReturn($content);
+
+        $action = new ResetProcessAction(
+            $resetForm->reveal(),
+            $this->prophesize(Services\AccountManagement::class)->reveal(),
+            $twig->reveal(),
+            $this->prophesize(Routing\Generator\UrlGeneratorInterface::class)->reveal()
+        );
+
+        $response = $action($request->reveal());
+
+        $this->assertInstanceOf(HttpFoundation\Response::class, $response);
+        $this->assertSame(HttpFoundation\Response::HTTP_OK, $response->getStatusCode());
+        $this->assertSame($content, $response->getContent());
+    }
+
+    public function testRendersFormIfFormIsSubmittedButNotValid()
+    {
+        $faker = $this->faker();
+
+        $userId    = $faker->numberBetween(1);
+        $resetCode = $faker->sha256;
+
+        $content = $faker->text;
+
+        $request = $this->prophesize(HttpFoundation\Request::class);
+
+        $request
+            ->get(Argument::exact('user_id'))
+            ->shouldBeCalled()
+            ->willReturn((string) $userId);
+
+        $request
+            ->get(Argument::exact('reset_code'))
+            ->shouldBeCalled()
+            ->willReturn($resetCode);
+
+        $resetFormView = $this->prophesize(Form\FormView::class);
+
+        $userElement = $this->prophesize(Form\FormInterface::class);
+
+        $userElement
+            ->setData(Argument::exact((string) $userId))
+            ->shouldBeCalled();
+
+        $resetCodeElement = $this->prophesize(Form\FormInterface::class);
+
+        $resetCodeElement
+            ->setData(Argument::exact($resetCode))
+            ->shouldBeCalled();
+
+        $resetForm = $this->prophesize(Form\FormInterface::class);
+
+        $resetForm
+            ->handleRequest(Argument::exact($request))
+            ->shouldBeCalled();
+
+        $resetForm
+            ->isSubmitted()
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $resetForm
+            ->isValid()
+            ->shouldBeCalled()
+            ->willReturn(false);
+
+        $resetForm
+            ->get(Argument::exact('user_id'))
+            ->shouldBeCalled()
+            ->willReturn($userElement);
+
+        $resetForm
+            ->get(Argument::exact('reset_code'))
+            ->shouldBeCalled()
+            ->willReturn($resetCodeElement);
+
+        $resetForm
+            ->createView()
+            ->shouldBeCalled()
+            ->willReturn($resetFormView);
+
+        $twig = $this->prophesize(Twig_Environment::class);
+
+        $twig
+            ->render(
+                Argument::exact('user/reset_password.twig'),
+                Argument::exact([
+                    'form' => $resetFormView->reveal(),
+                ])
+            )
+            ->shouldBeCalled()
+            ->willReturn($content);
+
+        $action = new ResetProcessAction(
+            $resetForm->reveal(),
+            $this->prophesize(Services\AccountManagement::class)->reveal(),
+            $twig->reveal(),
+            $this->prophesize(Routing\Generator\UrlGeneratorInterface::class)->reveal()
+        );
+
+        $response = $action($request->reveal());
+
+        $this->assertInstanceOf(HttpFoundation\Response::class, $response);
+        $this->assertSame(HttpFoundation\Response::HTTP_OK, $response->getStatusCode());
+        $this->assertSame($content, $response->getContent());
+    }
+
+    public function testRedirectsToForgotPasswordIfUserWasNotFound()
+    {
+        $faker = $this->faker();
+
+        $userId    = $faker->numberBetween(1);
+        $resetCode = $faker->sha256;
+
+        $url = $faker->url;
+
+        $session = $this->prophesize(HttpFoundation\Session\SessionInterface::class);
+
+        $session
+            ->set(
+                Argument::exact('flash'),
+                Argument::exact([
+                    'type'  => 'error',
+                    'short' => 'Error',
+                    'ext'   => 'The reset you have requested appears to be invalid, please try again.',
+                ])
+            )
+            ->shouldBeCalled();
+
+        $request = $this->prophesize(HttpFoundation\Request::class);
+
+        $request
+            ->get(Argument::exact('user_id'))
+            ->shouldBeCalled()
+            ->willReturn((string) $userId);
+
+        $request
+            ->get(Argument::exact('reset_code'))
+            ->shouldBeCalled()
+            ->willReturn($resetCode);
+
+        $request
+            ->getSession()
+            ->shouldBeCalled()
+            ->willReturn($session);
+
+        $resetForm = $this->prophesize(Form\FormInterface::class);
+
+        $resetForm
+            ->handleRequest(Argument::exact($request))
+            ->shouldBeCalled();
+
+        $resetForm
+            ->isSubmitted()
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $resetForm
+            ->isValid()
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $accountManagement = $this->prophesize(Services\AccountManagement::class);
+
+        $accountManagement
+            ->findById(Argument::exact($userId))
+            ->shouldBeCalled()
+            ->willThrow(new Auth\UserNotFoundException());
+
+        $urlGenerator = $this->prophesize(Routing\Generator\UrlGeneratorInterface::class);
+
+        $urlGenerator
+            ->generate(Argument::exact('forgot_password'))
+            ->shouldBeCalled()
+            ->willReturn($url);
+
+        $action = new ResetProcessAction(
+            $resetForm->reveal(),
+            $accountManagement->reveal(),
+            $this->prophesize(Twig_Environment::class)->reveal(),
+            $urlGenerator->reveal()
+        );
+
+        /** @var HttpFoundation\RedirectResponse $response */
+        $response = $action($request->reveal());
+
+        $this->assertInstanceOf(HttpFoundation\RedirectResponse::class, $response);
+        $this->assertSame(HttpFoundation\Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame($url, $response->getTargetUrl());
+    }
+
+    public function testRedirectsToForgotPasswordIfResetCodeIsInvalid()
+    {
+        $faker = $this->faker();
+
+        $userId    = $faker->numberBetween(1);
+        $resetCode = $faker->sha256;
+
+        $url = $faker->url;
+
+        $session = $this->prophesize(HttpFoundation\Session\SessionInterface::class);
+
+        $session
+            ->set(
+                Argument::exact('flash'),
+                Argument::exact([
+                    'type'  => 'error',
+                    'short' => 'Error',
+                    'ext'   => 'The reset you have requested appears to be invalid, please try again.',
+                ])
+            )
+            ->shouldBeCalled();
+
+        $request = $this->prophesize(HttpFoundation\Request::class);
+
+        $request
+            ->get(Argument::exact('user_id'))
+            ->shouldBeCalled()
+            ->willReturn((string) $userId);
+
+        $request
+            ->get(Argument::exact('reset_code'))
+            ->shouldBeCalled()
+            ->willReturn($resetCode);
+
+        $request
+            ->getSession()
+            ->shouldBeCalled()
+            ->willReturn($session);
+
+        $resetForm = $this->prophesize(Form\FormInterface::class);
+
+        $resetForm
+            ->handleRequest(Argument::exact($request))
+            ->shouldBeCalled();
+
+        $resetForm
+            ->isSubmitted()
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $resetForm
+            ->isValid()
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $user = $this->prophesize(Auth\UserInterface::class);
+
+        $user
+            ->checkResetPasswordCode(Argument::exact($resetCode))
+            ->shouldBeCalled()
+            ->willReturn(false);
+
+        $accountManagement = $this->prophesize(Services\AccountManagement::class);
+
+        $accountManagement
+            ->findById(Argument::exact($userId))
+            ->shouldBeCalled()
+            ->willReturn($user);
+
+        $urlGenerator = $this->prophesize(Routing\Generator\UrlGeneratorInterface::class);
+
+        $urlGenerator
+            ->generate(Argument::exact('forgot_password'))
+            ->shouldBeCalled()
+            ->willReturn($url);
+
+        $action = new ResetProcessAction(
+            $resetForm->reveal(),
+            $accountManagement->reveal(),
+            $this->prophesize(Twig_Environment::class)->reveal(),
+            $urlGenerator->reveal()
+        );
+
+        /** @var HttpFoundation\RedirectResponse $response */
+        $response = $action($request->reveal());
+
+        $this->assertInstanceOf(HttpFoundation\RedirectResponse::class, $response);
+        $this->assertSame(HttpFoundation\Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame($url, $response->getTargetUrl());
+    }
+
+    public function testRedirectsToForgotPasswordIfResetCodeIsValid()
+    {
+        $faker = $this->faker();
+
+        $userId    = $faker->numberBetween(1);
+        $resetCode = $faker->sha256;
+
+        $url = $faker->url;
+
+        $request = $this->prophesize(HttpFoundation\Request::class);
+
+        $request
+            ->get(Argument::exact('user_id'))
+            ->shouldBeCalled()
+            ->willReturn((string) $userId);
+
+        $request
+            ->get(Argument::exact('reset_code'))
+            ->shouldBeCalled()
+            ->willReturn($resetCode);
+
+        $resetForm = $this->prophesize(Form\FormInterface::class);
+
+        $resetForm
+            ->handleRequest(Argument::exact($request))
+            ->shouldBeCalled();
+
+        $resetForm
+            ->isSubmitted()
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $resetForm
+            ->isValid()
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $user = $this->prophesize(Auth\UserInterface::class);
+
+        $user
+            ->checkResetPasswordCode(Argument::exact($resetCode))
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $accountManagement = $this->prophesize(Services\AccountManagement::class);
+
+        $accountManagement
+            ->findById(Argument::exact($userId))
+            ->shouldBeCalled()
+            ->willReturn($user);
+
+        $urlGenerator = $this->prophesize(Routing\Generator\UrlGeneratorInterface::class);
+
+        $urlGenerator
+            ->generate(Argument::exact('forgot_password'))
+            ->shouldBeCalled()
+            ->willReturn($url);
+
+        $action = new ResetProcessAction(
+            $resetForm->reveal(),
+            $accountManagement->reveal(),
+            $this->prophesize(Twig_Environment::class)->reveal(),
+            $urlGenerator->reveal()
+        );
+
+        /** @var HttpFoundation\RedirectResponse $response */
+        $response = $action($request->reveal());
+
+        $this->assertInstanceOf(HttpFoundation\RedirectResponse::class, $response);
+        $this->assertSame(HttpFoundation\Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame($url, $response->getTargetUrl());
+    }
+}
