2b278fab942dc328781e8934de7f2c77bb3a8c35
BackEndTea <gdpagter@ibuildings.nl>
2018-01-01 13:46:49 +0100

    Happy new year

--- tests/Integration/Http/Action/Reviewer/Talk/IndexActionTest.php
+++ tests/Integration/Http/Action/Reviewer/Talk/IndexActionTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Copyright (c) 2013-2017 OpenCFP
+ * Copyright (c) 2013-2018 OpenCFP
  *
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
