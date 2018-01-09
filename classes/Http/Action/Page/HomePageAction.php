d0b50ddda54e0380a2747cada2500450826b366a
Gert de Pagter <gertp93@gmail.com>
2018-01-09 20:52:55 +0100

    Pass the model as a constructor argument
    This will allow us to mock out models easier.

--- classes/Http/Action/Page/HomePageAction.php
+++ classes/Http/Action/Page/HomePageAction.php
@@ -23,9 +23,15 @@ final class HomePageAction
      */
     private $showSubmissionCount;
 
-    public function __construct(bool $showSubmissionCount)
+    /**
+     * @var Talk
+     */
+    private $talk;
+
+    public function __construct(bool $showSubmissionCount, Talk $talk)
     {
         $this->showSubmissionCount = $showSubmissionCount;
+        $this->talk                = $talk;
     }
 
     /**
@@ -34,7 +40,7 @@ final class HomePageAction
     public function __invoke(): array
     {
         return [
-            'number_of_talks' => $this->showSubmissionCount ? Talk::count() : '',
+            'number_of_talks' => $this->showSubmissionCount ? $this->talk->count() : '',
         ];
     }
 }
